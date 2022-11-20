51f4dd9fee321b41cd3a82c151bab593c9fb0734
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-19 22:12:13 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
 import {useState} from 'react';
 
 function App() {
-  const [a, setA] = useState([1,2])
+  const [a, setA] = useState([1,2]);
+  
+  function clickButton() {
+    console.log("dddd");
+    let newA = a;
+    newA[1] = 9;
+    setA(newA);
+    console.log("***"+a);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Heading</h1>import {useState} from 'react';
-        
+        <h1>Heading</h1>
+        <button onClick={clickButton}>click</button>
         {a.map((letter) => (
           <p>{letter}</p>
         ))}
