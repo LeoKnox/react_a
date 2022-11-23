f22823d58ab25c2bf907a9169eab8868a3a531ed
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-22 23:23:55 -0800

    Add files via upload

--- app2/App.js
+++ app2/App.js
@@ -12,9 +12,9 @@ function App() {
   
   const clickButton = () => {
     console.log("dddd");
-    let newData = a;
-    newData.push(1);
-    setA(newData);
+    //let newData = a;
+    //newData.push(1);
+    setA(a.push(1));
     console.log(a);
   }
 
