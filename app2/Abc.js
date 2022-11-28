92e2ab09fd0dde1db7f283b8504ed51289032c21
LeoKnox <LeoKnox@users.noreply.github.com>
2022-11-27 22:33:43 -0800

    Add files via upload

--- app2/Abc.js
+++ app2/Abc.js
@@ -1,9 +1,13 @@
 export default function Abc(a) {
+    const newClick = (index) => {
+        console.log(index);
+    }
+
     console.log(JSON.stringify(a));
     return (
         <>
-        {a.a.map((b) => (
-            <p>{b}</p>
+        {a.a.map((b, index) => (
+            <p><button onClick={newClick(index)}>b</button>{b}</p>
         ))}
         </>
     )
