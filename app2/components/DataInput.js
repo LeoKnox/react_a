4bf87f8b6c445d06cbe1ec67915e63ef806b05a2
LeoKnox <LeoKnox@users.noreply.github.com>
2022-12-06 23:46:11 -0800

    Add files via upload

--- app2/components/DataInput.js
+++ app2/components/DataInput.js
@@ -2,6 +2,16 @@ import { useState } from 'react';
 
 const DataInput = ({createRoom}) => {
     const [room, setRoom] = useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (room === "") {
+            return console.log("add some rooms")
+        }
+        DataInput(room)
+        setRoom("");
+    }
+
     return (
         <div>Data input</div>
     )
