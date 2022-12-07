41f155a59bf88692c2cf12f7f47ba560a69911b9
LeoKnox <LeoKnox@users.noreply.github.com>
2022-12-06 23:50:19 -0800

    Add files via upload

--- app2/components/DataInput.js
+++ app2/components/DataInput.js
@@ -13,7 +13,11 @@ const DataInput = ({createRoom}) => {
     }
 
     return (
+        <>
+        <input type="text" placehold="create room" value={room} onChange={(e) => setRoom(e.target.value)} />
+        <button onClick={handleSubmit}>Create</button>
         <div>Data input</div>
+        </>
     )
 }
 
