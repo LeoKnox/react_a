28a027b8704f2107a1f71e7740fbd5e0183ced75
LeoKnox <LeoKnox@users.noreply.github.com>
2022-12-08 21:45:25 -0800

    Add files via upload

--- app2/components/DataItem.js
+++ app2/components/DataItem.js
@@ -1,9 +1,12 @@
-const DataItem = ({item, index}) => {
+const DataItem = ({item, index, deleteDataItem}) => {
     //console.log("data:"+JSON.stringify(item));
     console.log("data:"+item.room);
     return (
         <div>
             <li>{item.room}:{item.width}x{item.height}</li>
+            <>
+                <button onClick={() => deleteDataItem(index)}>X</button>
+            </>
         </div>
     )
 }
