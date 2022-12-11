d2b3f99718f1ef826adcf715881d98945aeb60f0
LeoKnox <LeoKnox@users.noreply.github.com>
2022-12-10 23:07:43 -0800

    Add files via upload

--- app2/components/DataInput.js
+++ app2/components/DataInput.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 const DataInput = ({createDataItem}) => {
     const [room, setRoom] = useState("");
     const [width, setWidth] = useState(1);
+    const [height, setHeight] = useState(1);
 
     const handleSubmit = e => {
         let roomObj = {}
@@ -13,6 +14,7 @@ const DataInput = ({createDataItem}) => {
         console.log("hs"+width)
         roomObj.room = room;
         roomObj.width = width;
+        roomObj.height = height;
         console.log("HHSS"+JSON.stringify(roomObj));
         createDataItem(roomObj);
         setRoom("");
@@ -22,6 +24,7 @@ const DataInput = ({createDataItem}) => {
         <form onSubmit={handleSubmit}>
         <input type="text" placehold="create room" value={room} onChange={(e) => setRoom(e.target.value)} />
         <input type="number" placeholder="1" value={width} onChange={(e) => setWidth(e.target.value)} />
+        <input type="number" value={height} onChange={(e) => setHeight(e.target.value)} />
         <button onClick={handleSubmit}>Create</button>
         <div>Data input</div>
         </form>
