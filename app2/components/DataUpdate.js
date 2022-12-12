278001309d8b7396767dd5cc7c4c21ac9546e23c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-12-11 23:28:07 -0800

    Add files via upload

--- app2/components/DataUpdate.js
+++ app2/components/DataUpdate.js
@@ -1,7 +1,10 @@
 import {useState} from 'react';
 
 const dataUpdate = ({createUpdateItem}) => {
+    console.log("tt"+createUpdateItem);
     return (
         <h1>update room</h1>
     )
 }
+
+export default dataUpdate;
