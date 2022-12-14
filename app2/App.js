5904cdc080e3a57eff71d4eafb8c6c433468d4b0
LeoKnox <LeoKnox@users.noreply.github.com>
2022-12-13 22:40:39 -0800

    Add files via upload

--- app2/App.js
+++ app2/App.js
@@ -69,7 +69,7 @@ function App() {
       {data.map((aaa, index) => (
         <>
         <DataItem key={index} index={index} item={aaa} whatDataItem={whatDataItem} updateDataItem={updateDataItem} deleteDataItem={deleteDataItem} completeDataItem={completeDataItem} />
-        <DataUpdate whatDataItem={(aaa) => whatDataItem} />
+        <DataUpdate index={index} item={aaa}whatDataItem={whatDataItem} />
         </>
       ))}
     </div>
