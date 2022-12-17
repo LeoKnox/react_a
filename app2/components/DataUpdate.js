82c250451b1bfad3ad48208d464a8c4438dab9e1
LeoKnox <LeoKnox@users.noreply.github.com>
2022-12-16 23:37:47 -0800

    Add files via upload

--- app2/components/DataUpdate.js
+++ app2/components/DataUpdate.js
@@ -1,12 +1,12 @@
 import {useState} from 'react';
 
 const DataUpdate = ({index, item, whatDataItem}) => {
-    const [room, setRoom] = useState("");
-    const [width, setWidth] = useState(index.width);
-    const [length, setLength] = useState("");
+    const [room, setRoom] = useState(item.room);
+    const [width, setWidth] = useState(item.width);
+    const [length, setLength] = useState(item.length);
 
     console.log("tt"+JSON.stringify(item));
-    console.log(index+"::"+whatDataItem);
+    console.log(width+"::"+whatDataItem);
     const handleSubmit = e => {
         e.preventDefault();
         setRoom(e.value);
@@ -16,9 +16,9 @@ const DataUpdate = ({index, item, whatDataItem}) => {
         <>
         <form onSubmit={handleSubmit}>
             <label>Rooom:</label>
-            <input type="text" value={item.room} onChange={(e) => setRoom(e.target.value)} />
+            <input type="text" value={room} onChange={(e) => setRoom(e.target.value)} />
             <label type="number" value={width} onChange={(e) => setWidth(e.target.value)}>Width:</label>
-            <input type="number" value={item.length} onChange={(e) => setLength(e.target.value)} />
+            <input type="number" value={length} onChange={(e) => setLength(e.target.value)} />
             <label>Length:</label>
             <input type="text" /><br/>
             <button onClick={() => whatDataItem(index)}>Click</button>
