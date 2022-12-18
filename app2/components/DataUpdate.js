34362a5d87e3173f72e58f330222c06202070b3d
LeoKnox <LeoKnox@users.noreply.github.com>
2022-12-17 22:03:11 -0800

    Add files via upload

--- app2/components/DataUpdate.js
+++ app2/components/DataUpdate.js
@@ -3,7 +3,7 @@ import {useState} from 'react';
 const DataUpdate = ({index, item, whatDataItem}) => {
     const [room, setRoom] = useState(item.room);
     const [width, setWidth] = useState(item.width);
-    const [length, setLength] = useState(item.length);
+    const [height, setHeight] = useState(item.Height);
 
     console.log("tt"+JSON.stringify(item));
     console.log(width+"::"+whatDataItem);
@@ -17,10 +17,10 @@ const DataUpdate = ({index, item, whatDataItem}) => {
         <form onSubmit={handleSubmit}>
             <label>Rooom:</label>
             <input type="text" value={room} onChange={(e) => setRoom(e.target.value)} />
-            <label type="text" value={width} onChange={(e) => setWidth(parseInt(e.target.value))}>Width:</label>
-            <input type="number" value={length} onChange={(e) => setLength(e.target.value)} />
+            <label>Width:</label>
+            <input type="number" value={width} onChange={(e) => setWidth(e.target.value)} />
             <label>Length:</label>
-            <input type="text" /><br/>
+            <input type="number" value={height} onChange={(e) => setHeight(e.target.value)} /><br/>
             <button onClick={() => whatDataItem(index)}>Click</button>
         </form>
         </>
