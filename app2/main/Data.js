cedd54464f044a9ff06f2bf50919d7440144486a
LeoKnox <LeoKnox@users.noreply.github.com>
2023-01-05 23:30:25 -0800

    Add files via upload

--- app2/main/Data.js
+++ app2/main/Data.js
@@ -1,3 +1,6 @@
-export default Data => {
+import {useEffect, useState} from 'react';
+
+export default data => {
+    const [creation, setCreation] = (useState([{a:1, b:2}, {a:5, b:6}]))
     return (<h1>Much data</h1>);
 };
