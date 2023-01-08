a1d9a89075c43d1ceb54c1fd9c8facc8b93804c1
LeoKnox <LeoKnox@users.noreply.github.com>
2023-01-07 22:55:43 -0800

    Add files via upload

--- app2/main/Data.js
+++ app2/main/Data.js
@@ -1,17 +1,19 @@
 import {useEffect, useState} from 'react';
 
 export default data => {
-    const [creation, setCreation] = (useState([{a:1, b:2}, {a:5, b:6}]))
+    const [creation, setCreation] = (useState([{a:1}, {a:5}]))
     const oneClick = () => {
         setCreation([{a:3}, {a:9}]);
     }
     return (
         <>
         <h1>Much data</h1>
-        {creation.map((a) => (
+        {creation.map((a, i) => (
+            <>
             <p>{a.a}</p>
+            <button onClick={oneClick} index={i}>red {i}</button>
+            </>
         ))}
-        <button onClick={oneClick}>red</button>
         </>
     );
 };
