7e9651c0a8a36872b527ae96ce93cb5f107c9f76
LeoKnox <LeoKnox@users.noreply.github.com>
2023-01-10 22:53:07 -0800

    Add files via upload

--- app2/main/createData.js
+++ app2/main/createData.js
@@ -1,9 +1,11 @@
 const createData = (createClick) => {
+    return (
         <form onClick={}>
             <label>x:</label>
             <label>y:</label>
             <button>Submit</button>
         </form>
+    )
 }
 
 export default createData;
