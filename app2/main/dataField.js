14858a9ffad9ac63b76692163c5167a95220640c
LeoKnox <LeoKnox@users.noreply.github.com>
2023-01-10 22:38:53 -0800

    Add files via upload

--- app2/main/dataField.js
+++ app2/main/dataField.js
@@ -2,9 +2,8 @@ const DataField = ({i, a, oneClick}) => {
     const j = i;
     return(
         <>
-        <label>{a.a}</label>=
-        <input type="Number" value={a.a} />
-        <button onClick={oneClick(j)} index={i}>red {i}</button>
+            <p>a:{a.a}::
+            b:{a.b}</p>
         </>
     );
 }
