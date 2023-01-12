0f226d69142f1635aac75c479faa311664483be6
LeoKnox <LeoKnox@users.noreply.github.com>
2023-01-11 23:09:40 -0800

    Add files via upload

--- app2/main/createData.js
+++ app2/main/createData.js
@@ -1,10 +1,15 @@
+import { useState } from 'react';
+
 const createData = (createClick) => {
+    const [myA, setA] = useState(1);
+    const [myB, setB] = useState(2);
+
     return (
         <form onClick={createClick}>
             <label>x:</label>
-            <input type="Number" />
+            <input type="Number" onChange={(e) => setA(e.target.value)} />
             <label>y:</label>
-            <input type="Number" />
+            <input type="Number" onChange={(e) => setB(e.target.value)} />
             <button>Submit</button>
         </form>
     )
