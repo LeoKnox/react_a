2b3e01aa73f9e4346cafa6805c73bf8ed5aee574
LeoKnox <LeoKnox@users.noreply.github.com>
2023-01-12 18:45:16 -0800

    Add files via upload

--- app2/main/Data.js
+++ app2/main/Data.js
@@ -10,7 +10,7 @@ export default data => {
     function createClick(x) {
         let inputData = creation;
         inputData.push({a:8,b:9});
-        setCreation(inputData);
+        return(setCreation(inputData));
         alert(JSON.stringify(creation));
     }
     return (
