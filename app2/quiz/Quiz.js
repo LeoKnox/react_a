ce0b29a9ebcc270c8083c205b67b581a5a7db908
LeoKnox <LeoKnox@users.noreply.github.com>
2023-01-13 23:29:46 -0800

    Add files via upload

--- app2/quiz/Quiz.js
+++ app2/quiz/Quiz.js
@@ -2,7 +2,12 @@ const Quiz = ({questions}) => {
     console.log(questions);
 
     return (
+        <>
         <h1>quiz me</h1>
+        {questions.map((q) => (
+            <p>{q.q}</p>
+        ))}
+        </>
     )
 };
 
