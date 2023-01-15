66783e6905f8003cb077e55e9e337d3fba649b94
LeoKnox <LeoKnox@users.noreply.github.com>
2023-01-14 23:49:10 -0800

    Add files via upload

--- app2/quiz/Quiz.js
+++ app2/quiz/Quiz.js
@@ -2,7 +2,7 @@ import {useState} from 'react';
 
 const Quiz = ({questions, answer}) => {
     console.log(JSON.stringify(answer)+"!!!!!!");
-    const [ans, setAns] = useState(questions[Math.floor(Math.random()*questions.length)]);
+    const [ans, setAns] = useState(questions[Math.floor(Math.random()*questions.length)-1]);
     console.log(ans.a+"++");
 
     return (
