473aab632754371b8796f91f1cc4adb758542819
LeoKnox <LeoKnox@users.noreply.github.com>
2023-01-15 21:36:16 -0800

    Add files via upload

--- app2/quiz/Quiz.js
+++ app2/quiz/Quiz.js
@@ -3,12 +3,16 @@ const Quiz = ({questions, answer}) => {
     const ans = questions[Math.floor(Math.random()*questions.length)].a;
     console.log(ans.a+"++");
 
+    function checkAns() {
+        console.log('red');
+    }
+
     return (
         <>
         <h1>quiz me</h1>
         <p>ans: {ans}</p>
         {questions.map((q) => (
-            <p>{q.q}</p>
+            <p onClick={checkAns}>{q.q}</p>
         ))}
         </>
     )
