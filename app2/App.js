ba9cdd85bb782422504e2f27fe27542cc3146f48
LeoKnox <LeoKnox@users.noreply.github.com>
2023-02-02 23:05:29 -0800

    Add files via upload

--- app2/App.js
+++ app2/App.js
@@ -39,6 +39,7 @@ function App() {
 function getQuiz(e) {
   //e.preventDeafult();
   let x = quiz.sort(() => Math.random() - Math.random()).slice(0, 4);
+  console.log("x:");
   console.log(x);
   setQuiz(x);
   //setQuiz(quiz.sort(() => Math.random() - Math.random()).slice(0, 4));
