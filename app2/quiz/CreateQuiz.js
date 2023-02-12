c27e101429dd0a193efab05f7589d7c0a1407e55
LeoKnox <LeoKnox@users.noreply.github.com>
2023-02-11 22:16:15 -0800

    Add files via upload

--- app2/quiz/CreateQuiz.js
+++ app2/quiz/CreateQuiz.js
@@ -1,29 +1,29 @@
 import {useState} from 'react';
 
 const CreateQuiz = ({addQuiz, getQuiz, quizData}) => {
-    const [newQuiz, setNewQuiz] = useState("z");
-    const [newAns, setNewAns] = useState("orage");
-    console.log("a:"+newQuiz);
+    const [q, setNewQuiz] = useState("z");
+    const [a, setNewAns] = useState("orage");
+    console.log("a:"+q);
 
     const changeForm = (e) => {
         e.preventDefault();
         console.log("x---");
         console.log(e.target.value);
         setNewQuiz(e.target.value);
-        console.log("="+newQuiz);
+        console.log("="+q);
     }
 
     const subQuiz = (e) => {
         e.preventDefault();
         let x= e.targetvalue;
-        addQuiz(newQuiz);
+        addQuiz({q, a});
     }
 
     return (
         <>
             <form onSubmit={subQuiz}>
-                <input type="text" value={newQuiz} onChange={changeForm} />
-                <input type="text" value={newAns} />
+                <input type="text" value={q} onChange={changeForm} />
+                <input type="text" value={a} />
                 <input type="submit" value="Add" />
             </form>
         </>
