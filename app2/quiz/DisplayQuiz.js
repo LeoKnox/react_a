505836eea35a49330f28608520f22528364acd99
LeoKnox <LeoKnox@users.noreply.github.com>
2023-02-13 22:32:17 -0800

    Add files via upload

--- app2/quiz/DisplayQuiz.js
+++ app2/quiz/DisplayQuiz.js
@@ -1,9 +1,9 @@
 import {useEffect} from 'react';
 
 const DisplayQuiz = ({quizes}) => {
-    const [myQuizes, setMyQuizes] = (useEffect(quizes));
+    const [myQuizes, setMyQuizes] = useEffect(quizes);
     console.log("quizes");
-    console.log(myQuizes);
+    console.log(quizes);
 
     return (
         <h1>sub quizes</h1>
