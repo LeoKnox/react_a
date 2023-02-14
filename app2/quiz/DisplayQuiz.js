3a6cfde01b03268be99becbf84ba286a38cbf919
LeoKnox <LeoKnox@users.noreply.github.com>
2023-02-13 22:42:33 -0800

    Add files via upload

--- app2/quiz/DisplayQuiz.js
+++ app2/quiz/DisplayQuiz.js
@@ -1,13 +1,28 @@
 import {useState} from 'react';
+import CreateQuiz from './quiz/CreateQuiz';
 
 const DisplayQuiz = ({quizes}) => {
     const [myQuizes, setMyQuizes] = useState(quizes);
     console.log("quizes");
     console.log(myQuizes);
 
+    function getQuiz(e) {
+    //e.preventDeafult();
+    //let x = qdOne.sort(() => Math.random() - Math.random()).slice(0, 4);
+    let x = 1;
+    setQuiz(x);
+    }
+
+    function addQuiz(pushQuiz) {
+    let x = myQuizes;
+    x.push(pushQuiz);
+    setQdOne(x);
+    }
+
     return (
         <>
         <h1>sub quizes</h1>
+        <CreateQuiz addQuiz={addQuiz} getQuiz={getQuiz} quiData={quizData} />
         {myQuizes.map((mq) => (
             <p>{mq.a}</p>
         ))}
