fc5947aec895b6e96558a9016e5945622294f020
LeoKnox <LeoKnox@users.noreply.github.com>
2023-02-14 22:41:31 -0800

    Add files via upload

--- app2/quiz/DisplayQuiz.js
+++ app2/quiz/DisplayQuiz.js
@@ -19,16 +19,16 @@ const DisplayQuiz = ({quizes}) => {
     setMyQuizes(x);
     }
 
-    function deleteQuiz() {
-        console.log("akai");
+    function deleteQuiz(index) {
+        console.log("akai"+index);
     }
 
     return (
         <>
         <h1>sub quizes</h1>
         <CreateQuiz addQuiz={addQuiz} quizData={myQuizes} />
-        {myQuizes.map((mq) => (
-            <p>answer:{mq.a} question:{mq.q}<button onClick={deleteQuiz}>Delete</button></p>
+        {myQuizes.map((mq, i) => (
+            <p>answer:{mq.a} question:{mq.q}<button onSubmit={deleteQuiz(i)}>Delete</button></p>
         ))}
         </>
     )
