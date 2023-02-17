11e2ea0104c884a2f0cd94174733af28ac28c68e
LeoKnox <LeoKnox@users.noreply.github.com>
2023-02-16 23:24:09 -0800

    Add files via upload

--- app2/quiz/DisplayQuiz.js
+++ app2/quiz/DisplayQuiz.js
@@ -29,8 +29,7 @@ const DisplayQuiz = ({quizes}) => {
         <CreateQuiz addQuiz={addQuiz} quizData={myQuizes} />
         {myQuizes.map((mq, i) => (
             <p>answer:{mq.a} question:{mq.q}
-                <button deleteQuiz={() => deleteQuiz}>Delete</button>
-                
+                <button deleteQuiz={(i) => deleteQuiz}>Delete</button>
             </p>
         ))}
         </>
