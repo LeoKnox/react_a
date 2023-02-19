e601071bbd6a7213e13716648958cee8ba67931e
LeoKnox <LeoKnox@users.noreply.github.com>
2023-02-18 23:50:11 -0800

    Add files via upload

--- app2/App.js
+++ app2/App.js
@@ -53,6 +53,7 @@ function addQuiz(pushQuiz) {
 
 function removeQuiz(i = 3) {
   console.log("removeIndex");
+  console.log(JSON.stringify(qdOne));
   let newQd = qdOne;
   //newQd = newQd.splice(i, 1);
   delete newQd[i];
