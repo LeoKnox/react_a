590a49222b71df2f57379f29bee2b7f005e89fd5
LeoKnox <LeoKnox@users.noreply.github.com>
2023-02-25 21:54:25 -0800

    Add files via upload

--- app2/App.js
+++ app2/App.js
@@ -1,14 +1,7 @@
 import './App.css';
 import {useEffect, useState, useReducer} from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import DataInput from "./components/DataInput";
-import DataItem from "./components/DataItem";
-import DataUpdate from "./components/DataUpdate";
-import Data from './main/Data';
-import Quiz from './quiz/Quiz';
-import CreateQuiz from './quiz/CreateQuiz';
-import DisplayQuiz from './quiz/DisplayQuiz';
-import Test from "./test";
+import KanjiList from "./pages/KanjiList";
 
 function App() {
   let quizData =
@@ -24,67 +17,17 @@ function App() {
       {q: "kyu", a:9, c:"ultraultraviolet"}
     ];
   const [qdOne, setQdOne] = useState(quizData);
-  const [a, setA] = useState([]);
   const [quiz, setQuiz] = useState(quizData.sort(() => Math.random() - Math.random()).slice(0, 4));
-  const [newQuiz, setNewQuiz] = useState(quiz[Math.floor(Math.random()*quiz.length)]);
-  const [data, setData] = useState(
-    [
-      {room: 'Entry', width:5, height:5},
-      {room: 'Blue', width:4, height:6},
-      {room: 'Red', width:8, height:7},
-    ]
-  )
-  const aData = [11,22];
 
   useEffect((event) => {
-    //event.preventDefault();
-    setA(aData)
-    getQuiz();
-    setQdOne(quizData)
-  }, [qdOne.c])
-
-function getQuiz(e) {
-  //e.preventDeafult();
-  let x = qdOne.sort(() => Math.random() - Math.random()).slice(0, 4);
-  setQuiz(x);
-}
-
-function addQuiz(pushQuiz) {
-  let x = qdOne;
-  x.push(pushQuiz);
-  setQdOne(x);
-};
-
-function removeQuiz(event) {
-  console.log("removeIndex");
-  console.log(JSON.stringify(qdOne));
-  let newQd = qdOne;
-  newQd.splice(event.target.value, 1);
-  console.log("spliced");
-  console.log(newQd);
-  //console.log(event.target.value);
-  //delete newQd[event.target.value];
-  console.log("newQd"+JSON.stringify(newQd));
-  setQdOne(newQd); 
-};
+  }, [])
 
   return (
     <div className="App">
       <h1>Heading</h1>
-      <Quiz questions={quiz} quizData={quizData} getQuiz={getQuiz} />
-      <CreateQuiz addQuiz={addQuiz} getQuiz={getQuiz} quizData={quizData} />
-      {qdOne.map((qd, i) => (
-        <p>{qd.a}:{qd.q}::{qd.c}
-          <button value={i} onClick={removeQuiz}>
-            Del
-          </button>
-        </p>
-      ))}
-      <Data />
       <BrowserRouter>
         <Routes>
-          <Route path="display" element={<DisplayQuiz quizes={qdOne} removeQuiz={removeQuiz} />}>
-          </Route>
+          <Route exact path="/" element={<KanjiList/>} />
         </Routes>
       </BrowserRouter>
     </div>
