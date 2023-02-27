c4a2aad355bc00088e23d451e99c36b18ba9b085
LeoKnox <LeoKnox@users.noreply.github.com>
2023-02-26 22:51:25 -0800

    Add files via upload

---
+++ app2/pages/Create.js
@@ -0,0 +1,7 @@
+function Create() {
+    return(
+        <p>Create List</p>
+    )
+}
+
+export default Create;
--- app2/pages/KanjiList.js
+++ app2/pages/KanjiList.js
@@ -1,6 +1,12 @@
+import {Outlet} from 'react-router-dom';
+import One from './One.js';
+
 function KanjiList() {
     return (
+        <>
         <p>List of Kanji</p>
+        <Outlet />
+        </>
     )
 }
 
