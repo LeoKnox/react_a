b5380d065e66f539b3ee73684ed4412c53f44410
LeoKnox <LeoKnox@users.noreply.github.com>
2023-02-27 23:54:37 -0800

    Add files via upload

--- app2/pages/KanjiList.js
+++ app2/pages/KanjiList.js
@@ -3,6 +3,7 @@ import {Outlet} from 'react-router-dom';
 import One from './One.js';
 
 function KanjiList() {
+    /*
   let quizData =
     [
       {q: "ichi", a:1, c:"red"},
@@ -14,9 +15,20 @@ function KanjiList() {
       {q: "nana", a:7, c:"violet"},
       {q: "hachi", a:8, c:"ultraviolet"},
       {q: "kyu", a:9, c:"ultraultraviolet"}
-    ];
+    ]; */
 
-    const [listKanji, setListKanji] = useState(quizData);
+    const [listKanji, setListKanji] = useState(
+    [
+      {q: "ichi", a:1, c:"red"},
+      {q: "ni", a:2, c:"orange"},
+      {q: "san", a:3, c:"yellow"},
+      {q: "yon", a:4, c:"green"},
+      {q: "go", a:5, c:"blue"},
+      {q: "roku", a:6, c:"indigo"},
+      {q: "nana", a:7, c:"violet"},
+      {q: "hachi", a:8, c:"ultraviolet"},
+      {q: "kyu", a:9, c:"ultraultraviolet"}
+    ]);
 
     useEffect((event) => {
         console.log(listKanji);
