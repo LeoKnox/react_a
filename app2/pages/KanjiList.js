9d65d6e5f7eb9bf2f7f92aa4dee4595efe39c057
LeoKnox <LeoKnox@users.noreply.github.com>
2023-03-02 23:02:13 -0800

    Add files via upload

--- app2/pages/KanjiList.js
+++ app2/pages/KanjiList.js
@@ -36,6 +36,7 @@ function KanjiList() {
         console.log(typeof tempKanji);
         tempKanji.push({q: "ju", a:777, c:"dnd"})
         tempKanji.pop();
+        setListKanji(tempKanji);
         if (listKanji === tempKanji) {
             console.log("equals!!!!!")
         }
