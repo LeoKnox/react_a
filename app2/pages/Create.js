cc038e69f88ffc0d783c79afadd407860df80a2f
LeoKnox <LeoKnox@users.noreply.github.com>
2023-03-08 22:52:02 -0800

    Add files via upload

--- app2/pages/Create.js
+++ app2/pages/Create.js
@@ -1,13 +1,13 @@
 import {useEffect, useState} from 'react';
 
 function Create({addKanji}) {
-    const [newQ, setNewQuiz] = useState("type here");
+    const [newQ, setNewQuiz] = useState({a:"type here"});
     const [newA, setNewAns] = useState("type here");
     const [newC, setNewCol] = useState("type here");
 
     return(
         <>
-        <input value={newQ} onChange={e => setNewQuiz(e.target.value)} type="text" />
+        <input value={newQ.a} onChange={e => setNewQuiz(e.target.value)} type="text" />
         <input value={newA} onChange={e => setNewAns(e.target.value)} type="text" />
         <input value={newC} onChange={e => setNewCol(e.target.value)} type="text" />
         <button onClick={addKanji} value={newQ}>add</button>
