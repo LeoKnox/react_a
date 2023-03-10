9e1a8627766ea81b8376b6da25a8b8f0f8e4599b
LeoKnox <LeoKnox@users.noreply.github.com>
2023-03-09 23:11:32 -0800

    Add files via upload

--- app2/pages/Create.js
+++ app2/pages/Create.js
@@ -5,16 +5,17 @@ function Create({addKanji}) {
     const [newA, setNewAns] = useState("type here");
     const [newC, setNewCol] = useState("type here");
 
-    function addKanji2(event) {
-        console.log(event);
+    function changeGeneric(event) {
+        console.log(event.target.id);
     }
 
     return(
         <>
+        <input id="z" value="red" onChange={changeGeneric} type="text" />
         <input id="q" value={newQ.q} onChange={e => setNewQuiz(e.target.value)} type="text" />
         <input value={newQ.a} onChange={e => setNewAns(e.target.value)} type="text" />
         <input value={newC} onChange={e => setNewCol(e.target.value)} type="text" />
-        <button onClick={addKanji2} value={newQ}>add</button>
+        <button onClick={addKanji} value={newQ}>add</button>
         </>
     )
 }
