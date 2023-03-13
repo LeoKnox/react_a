f766f371dc3051613fcaf3d39370e30f74ebccbd
LeoKnox <LeoKnox@users.noreply.github.com>
2023-03-12 23:28:08 -0700

    Add files via upload

--- app2/pages/Create.js
+++ app2/pages/Create.js
@@ -2,36 +2,37 @@ import {useEffect, useState} from 'react';
 
 function Create({addKanji}) {
     const [newQ, setNewQuiz] = useState("");
-    const [newA, setNewAns] = useState("type here");
-    const [newZ, setNewZ] = useState("");
-    const [newC, setNewCol] = useState("type here");
+    const [newA, setNewAns] = useState("");
+    const [newC, setNewCol] = useState("");
     const [generic, setGeneric] = useState({z:"", q:""});
 
     function changeGeneric(event) {
         let temp=generic;
         console.log('t');
         console.log(temp);
-        if (event.target.id=="z") {
-            setNewZ(event.target.value);
+        if (event.target.id=="a") {
+            setNewAns(event.target.value);
         };
         if (event.target.id=="q") {
             setNewQuiz(event.target.value);
         };
+        if (event.target.id=="c") {
+            setNewCol(event.target.value);
+        }
     }
 
     function submitKanji() {
         console.log("gg");
-        let temp ={z: newZ, q: newQ, a:"aa", c:"cc"};
+        let temp ={q: newQ, a:newA, c:newC};
         console.log(temp);
         addKanji(temp);
     }
 
     return(
         <>
-        <input id="z" value={newZ} onChange={changeGeneric} type="text" />
         <input id="q" value={newQ} onChange={changeGeneric} type="text" />
-        <input value={newQ.a} onChange={e => setNewAns(e.target.value)} type="text" />
-        <input value={newC} onChange={e => setNewCol(e.target.value)} type="text" />
+        <input id="a" value={newA} onChange={changeGeneric} type="text" />
+        <input id="c" value={newC} onChange={changeGeneric} type="text" />
         <button onClick={submitKanji} value={newQ}>add</button>
         </>
     )
