aea345e305671a5e492ae38f39e82374714d9921
LeoKnox <LeoKnox@users.noreply.github.com>
2023-03-14 22:37:05 -0700

    Add files via upload

--- app2/pages/Update.js
+++ app2/pages/Update.js
@@ -1,6 +1,9 @@
-import { useNavigate } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 
 function Update() {
+    const { kanjiId } = useLoaderData();
+    console.log(kanjiId);
+    
     return (
         <>
             <h3>update</h3>
