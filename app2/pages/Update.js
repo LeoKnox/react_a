d328319ffa9b406c99b4ee8ae40b3919e899cdc2
LeoKnox <LeoKnox@users.noreply.github.com>
2023-03-16 22:43:31 -0700

    Add files via upload

--- app2/pages/Update.js
+++ app2/pages/Update.js
@@ -1,13 +1,24 @@
 import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 function Update({oldKanji}) {
     const { kanjiId, newId } = useParams();
+    const {newQ, setQ} = useState("");
+    const {newC, setC} = useState("");
+    const {newA, setA} = useState("");
     console.log(oldKanji);
 
     return (
         <>
             <h3>update</h3>
             <button><a href={'/kanji/'}>Home</a></button>
+            <br/>
+            <label>Q:</label>
+            <input id="q" value={newQ} />
+            <label>C:</label>
+            <input id="c" value={newC} />
+            <label>A:</label>
+            <input id="a" value={newA} />
         </>
     )
 }
