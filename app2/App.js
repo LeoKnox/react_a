bf11562e27facc330dfeb25a9cb4d8e58b360fb2
LeoKnox <LeoKnox@users.noreply.github.com>
2023-03-26 23:29:44 -0700

    Add files via upload

--- app2/App.js
+++ app2/App.js
@@ -1,10 +1,17 @@
 import Home from "./kanjiQuiz/home.js";
+import Update from "./kanjiQuiz/update.js";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>Heading</h1>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="update" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ app2/kanjiQuiz/update.js
@@ -0,0 +1,9 @@
+function Update() {
+    return (
+        <>
+            <h3>update kanji</h3>
+        </>
+    )
+}
+
+export default Update;
