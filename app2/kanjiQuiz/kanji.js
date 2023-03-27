b8a28102f17f28e7d330f3f8b994845705c4b95a
LeoKnox <LeoKnox@users.noreply.github.com>
2023-03-26 22:02:27 -0700

    Add files via upload

--- app2/kanjiQuiz/kanji.js
+++ app2/kanjiQuiz/kanji.js
@@ -9,15 +9,15 @@ export function showKanji() {
     return(ourKanjis);
 }
 
+export function oneKanji() {
+    console.log("show one kanji");
+}
+
 export function addKanji(newKanji) {
     ourKanjis = [...ourKanjis, newKanji];
 }
 
 export function deleteKanji(id) {
-    console.log(typeof id);
-    let xid = 1;
-    console.log(id);
     const arr = ourKanjis.filter((item) => item.id !== Number(id));
-    console.log(arr);
     ourKanjis = arr;
 }
