7da57a220db6e024447b6008b06d519d170ee927
LeoKnox <LeoKnox@users.noreply.github.com>
2023-03-27 21:19:56 -0700

    Add files via upload

--- app2/kanjiQuiz/update.js
+++ app2/kanjiQuiz/update.js
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import { useParams } from 'react-router-dom';
 import { oneKanji } from "./kanji";
 
 function Update() {
     const {id} = useParams();
     const single = oneKanji(id);
+    const [q, setQ] = useState(single.q);
+
     console.log(single);
+
+    function updateEntry() {
+        console.log("update entry");
+    }
 
     return (
         <>
             <h1>One Kanji</h1>
+            <p>
+                Q:{single.q}
+                <input type="text" name="ques" value={q} onChange={updateEntry} />
+            </p>
+            <p>
+                A:{single.a}
+            </p>
+            <p>
+                I:{single.id}
+            </p>
         </>
     )
 }
