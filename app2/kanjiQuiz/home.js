0a422761252f39be44f0b5f191e2ef14d653c1c7
LeoKnox <LeoKnox@users.noreply.github.com>
2023-03-29 22:16:54 -0700

    Add files via upload

--- app2/kanjiQuiz/home.js
+++ app2/kanjiQuiz/home.js
@@ -10,7 +10,7 @@ function Home() {
 
     useEffect(() => {
       setSK(showKanji());
-      setId(sk.length);
+      setId(5);
       console.log("iii"+i);
     }, [])
 
@@ -51,7 +51,7 @@ function Home() {
         <label>&nbsp;question:</label>
         <input type="text" name="ques" value={q} onChange={updateEntry} />
         <label>&nbsp;id:</label>
-        <input type="number" name="id" value={i} />
+        <input type="number" name="id" value={i || 3} />
       </p>
       <button onClick={updateKanji}>Add</button>
       
