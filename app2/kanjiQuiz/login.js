f1e09e68e094ec62633ae8860a062108645d06e3
LeoKnox <LeoKnox@users.noreply.github.com>
2023-04-03 23:36:22 -0700

    Add files via upload

--- app2/kanjiQuiz/login.js
+++ app2/kanjiQuiz/login.js
@@ -1,10 +1,12 @@
 import { confirmUser } from "./user.js";
+import { Outlet } from 'react-router-dom';
 
 function Login() {
     return (
         <>
             <h3>Login page</h3>
             <button onClick={confirmUser()}>Login</button>
+            <Outlet />
         </>
     )
 }
