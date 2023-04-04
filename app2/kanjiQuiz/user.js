6e0aae8400ed5349c4d150a6c675108bdedfe2c3
LeoKnox <LeoKnox@users.noreply.github.com>
2023-04-03 22:55:39 -0700

    Add files via upload

---
+++ app2/kanjiQuiz/user.js
@@ -0,0 +1,10 @@
+let Users =
+    [
+        {useName:"use1", password:"pass1", kanjiNum:[1,3]},
+        {useName:"use2", password:"pass2", kanjiNum:[2,4]},
+        {useName:"use3", password:"pass3", kanjiNum:[1,5,8]}
+    ]
+
+export function confirmUser() {
+    return(<p>user confirmed</p>)
+}
