67c666d2da2d3c61c304ed58f342b53dbe07368c
LeoKnox <LeoKnox@users.noreply.github.com>
2023-04-17 21:29:27 -0700

    Add files via upload

---
+++ kanjiQuiz/user.js
@@ -0,0 +1,16 @@
+let Users =
+    [
+        {useName:"use1", password:"pass1", kanjiNum:[1,3]},
+        {useName:"use2", password:"pass2", kanjiNum:[2,4]},
+        {useName:"use3", password:"pass3", kanjiNum:[1,5,8]}
+    ]
+
+export function confirmUser(test, event) {
+    alert("madzu");
+    let x = Users.find((u) => u.useName == test[0]); 
+    console.log(x);
+    alert(test[0]);
+    alert(Users[0].useName);
+    alert("red");
+    //event.preventDefault();
+}
