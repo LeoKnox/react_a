f86c3a7e0df534b8d28037f254312dcb2f82aaac
LeoKnox <LeoKnox@users.noreply.github.com>
2023-04-20 23:20:43 -0700

    Add files via upload

--- app2/kanjiQuiz/login.js
+++ app2/kanjiQuiz/login.js
@@ -24,7 +24,7 @@ function Login() {
         console.log("user");
         console.log(u);
         confirmUser(u, p);
-        navigate('/logged');
+        navigate('/logged', {u});
         //confirmUser(user);
     }
     return (
--- app2/kanjiQuiz/mypage.js
+++ app2/kanjiQuiz/mypage.js
@@ -1,4 +1,6 @@
-function Mypage() {
+function Mypage(u) {
+    console.log("xx");
+    console.log(u);
     return(
         <h3>I'm loggged in</h3>
     )
