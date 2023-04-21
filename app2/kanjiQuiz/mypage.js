24879646d8464ba9bff4f83ffefa085f1c30fa92
LeoKnox <LeoKnox@users.noreply.github.com>
2023-04-20 23:49:37 -0700

    Add files via upload

--- app2/kanjiQuiz/mypage.js
+++ app2/kanjiQuiz/mypage.js
@@ -6,9 +6,7 @@ function Mypage({user}) {
     return(
         <>
         <h3>I'm loggged in</h3>
-        {user[kanjiNum].map((uk) => (
-            <p>{uk}</p>
-        ))}
+        <p>{user.useName}</p>
         </>
     )
 }
