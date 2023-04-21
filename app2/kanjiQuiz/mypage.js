65d6044acbb9b6f65e6cc46b7ca6d67f7d89cf45
LeoKnox <LeoKnox@users.noreply.github.com>
2023-04-20 23:35:22 -0700

    Add files via upload

--- app2/kanjiQuiz/mypage.js
+++ app2/kanjiQuiz/mypage.js
@@ -1,8 +1,15 @@
+import { showKanji } from "./kanji";
+
 function Mypage({user}) {
     console.log("xx");
     console.log(user);
     return(
+        <>
         <h3>I'm loggged in</h3>
+        {user.kanjiNum.map((uk) => (
+            <p>{uk}</p>
+        ))}
+        </>
     )
 }
 
