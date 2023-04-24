7e70e58c7834cbfab89e1d1ad81e26067b8682e0
LeoKnox <LeoKnox@users.noreply.github.com>
2023-04-23 23:00:46 -0700

    Add files via upload

--- app2/kanjiQuiz/mypage.js
+++ app2/kanjiQuiz/mypage.js
@@ -1,15 +1,16 @@
 import { showKanji } from "./kanji";
+import { useState} from "react";
 import { singleUser } from "./user.js";
 import { useParams } from 'react-router-dom';
 
 function Mypage({user}) {
     const { id } = useParams();
     console.log("xx");
-    console.log(singleUser(0));
+    const [myInfo] = useState(singleUser(id));
     return(
         <>
         <h3>I'm loggged in</h3>
-        <p>{id}</p>
+        <p>Welcome {myInfo.useName}</p>
         </>
     )
 }
