fcef30de048d5b93b0c39830553416e5717b990b
LeoKnox <LeoKnox@users.noreply.github.com>
2023-04-24 23:12:01 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,63 +1,27 @@
-import './App.css';
-import {useEffect, useState, useReducer} from 'react';
-import DataInput from "./components/DataInput";
-import DataItem from "./components/DataItem";
-import DataUpdate from "./components/DataUpdate";
-import Data from './main/Data';
-import Quiz from './quiz/Quiz';
-import CreateQuiz from './quiz/CreateQuiz';
-import Test from "./test";
+import Home from "./kanjiQuiz/home.js";
+import Test from "./kanjiQuiz/test.js";
+import Update from "./kanjiQuiz/update.js";
+import Login from "./kanjiQuiz/login.js";
+import Mypage from "./kanjiQuiz/mypage.js";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  let quizData =
-    [
-      {q: "ichi", a:1, c:"red"},
-      {q: "ni", a:2, c:"orange"},
-      {q: "san", a:3, c:"yellow"},
-      {q: "yon", a:4, c:"green"},
-      {q: "go", a:5, c:"blue"},
-      {q: "roku", a:6, c:"indigo"},
-      {q: "nana", a:7, c:"violet"}
-    ];
-  const [a, setA] = useState([]);
-  const [quiz, setQuiz] = useState(quizData.sort(() => Math.random() - Math.random()).slice(0, 4));
-  const [newQuiz, setNewQuiz] = useState(quiz[Math.floor(Math.random()*quiz.length)]);
-  const [data, setData] = useState(
-    [
-      {room: 'Entry', width:5, height:5},
-      {room: 'Blue', width:4, height:6},
-      {room: 'Red', width:8, height:7},
-    ]
-  )
-  const aData = [11,22];
-
-  useEffect(() => {
-    setA(aData)
-    getQuiz();
-  }, [])
-
-function getQuiz() {
-  return(setQuiz(quiz.sort(() => Math.random() - Math.random()).slice(0, 4)));
-}
-
-function addQuiz(pushQuiz) {
-  console.log("adding quiz!!!!!"+pushQuiz);
-  let x = quiz;
-  x.push({a:pushQuiz});
-  setQuiz(x);
-  quizData.push({a:pushQuiz});
-  console.log("last"+JSON.stringify(quiz));
-}
-
   return (
     <div className="App">
       <h1>Heading</h1>
-      <Quiz questions={quiz} quizData={quizData} getQuiz={(e) => getQuiz} />
-      <CreateQuiz addQuiz={addQuiz} />
-      {quizData.map((qd) => (
-        <p>{qd.a}:{qd.q}::{qd.c}</p>
-      ))}
-      <Data />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />}>
+            <Route path=":user" element={<p>tt</p>} />
+          </Route>
+          <Route path="/home" element={<Home />} />
+          <Route path="/logged/:id" element={<Mypage />} />
+          <Route path="update/:id" element={<Update />} />
+          <Route path="logged" element={<Mypage />} />
+          <Route path="test" element={<Test />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
