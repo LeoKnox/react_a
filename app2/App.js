82fa9c92be8bbda9a4ea0564ac132d19b32790c6
LeoKnox <LeoKnox@users.noreply.github.com>
2023-04-25 22:25:30 -0700

    Add files via upload

--- app2/App.js
+++ app2/App.js
@@ -4,6 +4,8 @@ import TestParam from "./kanjiQuiz/testParam.js";
 import Update from "./kanjiQuiz/update.js";
 import Login from "./kanjiQuiz/login.js";
 import Mypage from "./kanjiQuiz/mypage.js";
+import Nested from "./components/Nested";
+import NestedP from "./components/NestedP";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -22,6 +24,9 @@ function App() {
           <Route path="logged" element={<Mypage />} />
           <Route path="test" element={<Test />} />
           <Route path="test/:id" element={<TestParam />} />
+          <Route path="/nested" element={<Nested />}>
+            <Route path=":id" element={<NestedP />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
