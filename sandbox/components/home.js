efa370f07313fa4f4057bfeb61c18b862dbf2cb2
LeoKnox <LeoKnox@users.noreply.github.com>
2023-08-11 21:31:20 -0700

    Update home.js

--- sandbox/components/home.js
+++ sandbox/components/home.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
+
 const Home = () => {
   const dungeon = [
     { id: 1, name: "entry", width: 5, length: 5 },
     { id: 2, name: "orc_one", width: 8, length: 6 },
     { id: 3, name: "treasure_one", width: 7, length: 4 }
   ];
+  const [firstState, setFirstState] = useState("red");
+
+  function firstClick() {
+    setFirstState("blue");
+    console.log(firstState);
+  }
+
   return (
     <div>
       <h1>I'm Home</h1>
+      <h3>{firstState}</h3>
+      <button onClick={() => firstClick()}>click</button>
       {dungeon.map((d) => (
         <p>{d.name}</p>
       ))}
