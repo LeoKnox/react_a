5cb8cc25ef6951b554d72bf6dc177226c4f67e85
LeoKnox <LeoKnox@users.noreply.github.com>
2023-08-12 20:48:40 -0700

    Update firstComp.js

--- sandbox/components/firstComp.js
+++ sandbox/components/firstComp.js
@@ -1,5 +1,9 @@
-const FirstComp = () => {
-  return <h3>first comp</h3>;
+const FirstComp = ({ changeState }) => {
+  function test() {
+    changeState("blue");
+    alert("blue");
+  }
+  return <button onClick={changeState}>first comp</button>;
 };
 
 export default FirstComp;
