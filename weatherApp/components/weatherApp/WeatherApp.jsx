b9bbe0ac944257d32429dde94e196fbe827164e6
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-09 19:20:07 -0700

    Update WeatherApp.jsx

--- weatherApp/components/weatherApp/WeatherApp.jsx
+++ weatherApp/components/weatherApp/WeatherApp.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import './WeatherApp.css';
 
 const WeatherApp = () => {
+  const search = () => {
+    const element = document.getElementsByuClassName("CittyInput")
+    if (element[0].value==="")
+    {
+      return 0;
+    }
+    
+  }
   return (
     <div className='container'>
       <div className='top-bar'>
-        <input type="text" className="cityINput" placeholder="Mars" />
+        <input type="text" className="cityInput" placeholder="Mars" />
       </div>
       <div className="weather-temp">24f</div>
       <div className="weather-location">Sapporo</div>
