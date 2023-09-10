1f47eed0389a1b516ce15665013076cf1aa3b699
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-09 19:12:47 -0700

    Update WeatherApp.jsx

--- weatherApp/components/weatherApp/WeatherApp.jsx
+++ weatherApp/components/weatherApp/WeatherApp.jsx
@@ -7,6 +7,18 @@ const WeatherApp = () => {
       <div className='top-bar'>
         <input type="text" className="cityINput" placeholder="Mars" />
       </div>
+      <div className="weather-temp">24f</div>
+      <div className="weather-location">Sapporo</div>
+      <div className="data-container">
+        <div className="element">
+          <div className="humidty-percent">64%</div>
+          <div className="text">Humidity</div>
+        </div>
+        <div className="element">
+          <div className="humidty-percent">5 mph</div>
+          <div className="text">Wind speed</div>
+        </div>
+      </div>
     </div>
   )
 }
