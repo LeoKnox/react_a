9150a7406049739042431cefd989e7f6dfb32fcb
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-10 21:55:43 -0700

    Update Nav.js

--- Dungeon/components/Nav.js
+++ Dungeon/components/Nav.js
@@ -2,14 +2,13 @@ import {Room} from './Nav.js';
 
 const Nav = ({changePage}) => {
   function changeRoom() {
-    setCurrPage(<Room />)
     alert("red");
   }
 
   return (
     <div>
       <button>Home</button>
-      <button onClick={changePage}>Room</button>
+      <button onClick={() => changePage(<Room />)}>Room</button>
     </div>
   )
 }
