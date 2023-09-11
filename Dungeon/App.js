aaf1763a78534101ffe997fc72c4629b75255bae
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-10 21:14:51 -0700

    Update App.js

--- Dungeon/App.js
+++ Dungeon/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
+import Home from './components/Home.js';
 
 export default function App() {
   return (
     <div className="App">
       <h1>Build Dungeons</h1>
+      <Home />
     </div>
   );
 }
