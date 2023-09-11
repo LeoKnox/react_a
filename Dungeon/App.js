d9dba70f9214e3747c0e7fc75e252dc87e35c13c
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-10 21:17:35 -0700

    Update App.js

--- Dungeon/App.js
+++ Dungeon/App.js
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import "./styles.css";
 import Home from './components/Home.js';
 
 export default function App() {
+  const [currPage, setCurrPage] = useState(<Home />)
   return (
     <div className="App">
       <h1>Build Dungeons</h1>
-      <Home />
+      {currPage}
     </div>
   );
 }
