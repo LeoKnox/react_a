2fd263dfc7c4d127496f4ef411d556fe03dfc4a7
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-11 23:19:34 -0700

    Update Nav.js

--- Dungeon/components/Nav.js
+++ Dungeon/components/Nav.js
@@ -1,11 +1,12 @@
 import Home from './Home.js';
+import Room from './Room.js';
 import { useState } from 'react';
 
 const Nav = () => {
   const [page, setPage] = useState(<Home />)
   return (
     <>
-    <p>New Nav</p>
+    <p onClick={() => setPage(<Room />)}>New Nav</p>
     {page}
     </>
   )
