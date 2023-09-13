a840fc5c6653c02e8e943da90f35fe6c168dba93
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-12 23:10:34 -0700

    Update App.js

--- Dungeon/App.js
+++ Dungeon/App.js
@@ -2,14 +2,9 @@ import { useState } from 'react';
 import "./styles.css";
 import Home from './components/Home.js';
 import Room from './components/Room.js';
-import Nav from './components/Nav.js';
 
 export default function App() {
   const [currPage, setCurrPage] = useState(<Home />)
-
-  function changePage(x) {
-    setCurrPage(x)
-  }
 
   return (
     <div className="App">
