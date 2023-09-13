224bb967c16a925b00644c17078d140c1df19494
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-12 23:20:36 -0700

    Create roomData.js

---
+++ Dungeon/components/roomData.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import "./styles.css";
+import Home from './components/Home.js';
+import Room from './components/Room.js';
+import displayAll from './components/roomData.js';
+
+export default function App() {
+  const [currPage, setCurrPage] = useState(<Home />)
+
+  return (
+    <div className="App">
+      <h1>Build Dungeons</h1>
+      <div className="nav">
+        <button onClick={()=>setCurrPage(<Home />)}>Home</button>
+        <button onClick={()=>setCurrPage(<Room />)}>Room</button>
+      </div>
+      {currPage}
+      {displayAll}
+    </div>
+  );
+}
