2e012f3df25a67e4eacab90271c40c6c2ba5aefa
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-13 21:53:46 -0700

    Update App.js

--- Dungeon/App.js
+++ Dungeon/App.js
@@ -1,21 +1,21 @@
-import { useState } from 'react';
-import './App.css';
-import Home from './components/Home.js';
-import Room from './components/Room.js';
-import {displayAll} from './components/roomData.js';
+import { useState } from "react";
+import "./styles.css";
+import Home from "./components/Home.js";
+import Room from "./components/Room.js";
+import { displayAll } from "./components/roomData.js";
 
 export default function App() {
-  const [currPage, setCurrPage] = useState(<Home />)
+  const [currPage, setCurrPage] = useState(<Home />);
 
   return (
     <div className="App">
       <h1>Build Dungeons</h1>
       <div className="nav">
-        <button onClick={()=>setCurrPage(<Home />)}>Home</button>
-        <button onClick={()=>setCurrPage(<Room />)}>Room</button>
+        <button onClick={() => setCurrPage(<Home />)}>Home</button>
+        <button onClick={() => setCurrPage(<Room />)}>Room</button>
       </div>
       {currPage}
-      {displayAll()}
+      {displayAll().dungeon[0].name}
     </div>
   );
 }
