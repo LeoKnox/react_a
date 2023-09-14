24e84ac88610f7afc53f05a25fa1fa14f0ee3692
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-13 21:22:48 -0700

    Update App.js

--- Dungeon/App.js
+++ Dungeon/App.js
@@ -15,7 +15,7 @@ export default function App() {
         <button onClick={()=>setCurrPage(<Room />)}>Room</button>
       </div>
       {currPage}
-      {displayAll}
+      {displayAll()}
     </div>
   );
 }
