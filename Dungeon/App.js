44255ca4836389dd5c150f89fe4952673ca10207
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-14 21:15:37 -0700

    Update App.js

--- Dungeon/App.js
+++ Dungeon/App.js
@@ -15,22 +15,6 @@ export default function App() {
         <button onClick={() => setCurrPage(<Room />)}>Room</button>
       </div>
       {currPage}
-      <table>
-        <tr>
-          <th>Room Name</th>
-          <th>Description</th>
-          <th>Width</th>
-          <th>Length</th>
-        </tr>
-      {displayAll().dungeon.map((room) => (
-        <tr>
-          <td>{room.name}</td>
-          <td>{room.description}</td>
-          <td>{room.width}</td>
-          <td>{room.length}</td>
-        </tr>
-      ))}
-      </table>
     </div>
   );
 }
