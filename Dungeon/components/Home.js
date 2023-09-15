33543a5efc6c886a580c46f76d08924d71175b23
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-14 21:16:25 -0700

    Update Home.js

--- Dungeon/components/Home.js
+++ Dungeon/components/Home.js
@@ -1,6 +1,25 @@
+import {displayAll} from './roomData.js';
+
 const Home = () => {
   return (
-    <p>My Home Page</p>
+    <div className="Home">
+    <table>
+        <tr>
+          <th>Room Name</th>
+          <th>Description</th>
+          <th>Width</th>
+          <th>Length</th>
+        </tr>
+      {displayAll().dungeon.map((room) => (
+        <tr>
+          <td>{room.name}</td>
+          <td>{room.description}</td>
+          <td>{room.width}</td>
+          <td>{room.length}</td>
+        </tr>
+      ))}
+    </table>
+    </div>
   )
 }
 
