b92c64bc8914290d5cacdb71c037b87c7f2057ed
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-17 22:52:53 -0700

    Update roomData.js

--- Dungeon/components/roomData.js
+++ Dungeon/components/roomData.js
@@ -1,21 +1,9 @@
-import { useState } from "react";
-import "./styles.css";
-import Home from "./components/Home.js";
-import Room from "./components/Room.js";
-import { displayAll } from "./components/roomData.js";
+let roomData = [
+  {name:"Entry", description:"Entrance", width:5, lenght:5},
+  {name:"Throne", description:"Throne room", width:7, lenght:6},
+  {name:"Guard", description:"Guard room", width:6, lenght:8}
+]
 
-export default function App() {
-  const [currPage, setCurrPage] = useState(<Home />);
-
-  return (
-    <div className="App">
-      <h1>Build Dungeons</h1>
-      <div className="nav">
-        <button onClick={() => setCurrPage(<Home />)}>Home</button>
-        <button onClick={() => setCurrPage(<Room />)}>Room</button>
-      </div>
-      {currPage}
-      {displayAll().dungeon}
-    </div>
-  );
+export displayAll() {
+  console.log("all rooms");
 }
