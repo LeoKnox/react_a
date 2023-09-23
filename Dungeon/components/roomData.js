1350b84b8da5f196d8dd41aed6863e347c6527e6
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-22 21:23:31 -0700

    Update roomData.js

--- Dungeon/components/roomData.js
+++ Dungeon/components/roomData.js
@@ -1,9 +1,13 @@
 let roomData = [
-  {name:"Entry", description:"Entrance", width:5, length:5},
-  {name:"Throne", description:"Throne room", width:7, length:6},
-  {name:"Guard", description:"Guard room", width:6, length:8}
+  {id: 1, name:"Entry", description:"Entrance", width:5, length:5},
+  {id: 2, name:"Throne", description:"Throne room", width:7, length:6},
+  {id: 3, name:"Guard", description:"Guard room", width:6, length:8}
 ]
 
 export function displayAll() {
   return roomData
+}
+
+export function displayOne() {
+  return roomData[1].name
 }
