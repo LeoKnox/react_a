06397e2cac97deb0ad8416f825e45d0d0611fff0
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-23 20:55:33 -0700

    Update Home.js

--- Dungeon/components/Home.js
+++ Dungeon/components/Home.js
@@ -1,31 +1,13 @@
-import { displayAll } from "./roomData.js";
+import {displayOne} from './roomData.js';
 
-const Home = ({ changePage }) => {
-  const newPage = (x) => {
-    changePage()
-  }
-
+const Room = () => {
+  let currentRoom = {displayOne()};
   return (
-    <div className="Home">
-      <table>
-        <tr>
-          <th>Room Name</th>
-          <th>Description</th>
-          <th>Width</th>
-          <th>Length</th>
-        </tr>
-        {displayAll().map(room =>(
-          <tr>
-          <td>{room.name}</td>
-          <td>{room.description}</td>
-          <td>{room.width}</td>
-          <td>{room.length}</td>
-          <td onClick={() => newPage(room.id)}>Room</td>
-          </tr>
-        ))}
-      </table>
-    </div>
-  );
-};
+    <>
+    <p>Room page</p>
+    <p>Name: {currentRoom.name}</p>
+    </>
+  )
+}
 
-export default Home;
+export default Room;
