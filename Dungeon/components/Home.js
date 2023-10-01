026c4c3202c609068f1fb11b2c92414888797a95
LeoKnox <LeoKnox@users.noreply.github.com>
2023-09-30 21:13:57 -0700

    Update Home.js

--- Dungeon/components/Home.js
+++ Dungeon/components/Home.js
@@ -22,7 +22,7 @@ const Home = ({changePage}) => {
           <td>{room.description}</td>
           <td>{room.width}</td>
           <td>{room.length}</td>
-          <td onClick={newPage(room.id)}>Room {room.id}</td>
+          <td onClick={() => newPage(room.id)}>Room {room.id}</td>
           </tr>
         ))}
       </table>
