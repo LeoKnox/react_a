4408afd92cc33f7a9e830fb9837b11fce6bf8663
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-01 23:23:34 -0700

    Update Home.js

--- Dungeon/components/Home.js
+++ Dungeon/components/Home.js
@@ -1,27 +1,34 @@
-import { useState } from "react";
-//import "./styles.css";
-import Home from "./components/Home.js";
-import Room from "./components/Room.js";
-import { displayAll } from "./components/roomData.js";
+import { displayAll, displayOne } from "./roomData.js";
 
-export default function App() {
-  const [currPage, setCurrPage] = useState(<Home />);
-
-  //const changePage = () => {
-  function changePage() {
-    let x = 1
-    alert("change page " + x)
-    setCurrPage(<Room roomId={x} />)
+const Home = ({changePage}) => {
+  const newPage = (x) => {
+    alert("new page " + x)
+    //alert(changePage())
+    changePage()
   }
 
   return (
-    <div className="App">
-      <h1>Build Dungeons</h1>
-      <div className="nav">
-        <button onClick={() => setCurrPage(<Home changePage={() => changePage()} />)}>Home</button>
-        <button onClick={() => setCurrPage(<Room />)}>Room</button>
-      </div>
-      {currPage}
+    <div className="Home">
+      <button onClick={() => changePage}>click</button>
+      <table>
+        <tr>
+          <th>Room Name</th>
+          <th>Description</th>
+          <th>Width</th>
+          <th>Length</th>
+        </tr>
+        {displayAll().map(room =>(
+          <tr>
+          <td>{room.name}</td>
+          <td>{room.description}</td>
+          <td>{room.width}</td>
+          <td>{room.length}</td>
+          <td onClick={() => newPage(room.id)}>Room {room.id}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
-}
+};
+
+export default Home;
