fe592dd080afe34ec506974152cdd2912ececedc
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-02 21:43:26 -0700

    Update Home.js

--- dungeron_a/components/Home.js
+++ dungeron_a/components/Home.js
@@ -1,7 +1,15 @@
+import All from "./All.js";
+
 const Home = () => {
+  const roomData = [
+    {name: "entry", description: "enter", width: 5, length:5 },
+    {name: "throne", description: "King", width: 9, length:7 },
+    {name: "guard", description: "Guards", width: 8, length:6 }
+  ]
+
   return (
     <div>
-      Home
+      <All />
     </div>
   )
 }
