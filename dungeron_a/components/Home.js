46dbdd0eea6e5e6032267ef5a0f149bad4a51431
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-03 21:28:36 -0700

    Update Home.js

--- dungeron_a/components/Home.js
+++ dungeron_a/components/Home.js
@@ -2,12 +2,12 @@ import {useState} from "react";
 import All from "./All.js";
 
 const Home = () => {
-  const [current, setCurrent] = useState(<All />);
   const roomData = [
     {name: "entry", description: "enter", width: 5, length:5 },
     {name: "throne", description: "King", width: 9, length:7 },
     {name: "guard", description: "Guards", width: 8, length:6 }
   ]
+  const [current, setCurrent] = useState(<All rooms={roomData} />);
 
   return (
     <div>
