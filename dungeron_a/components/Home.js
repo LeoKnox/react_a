a0614f49538015bd95848ca22f563b9e75729fc6
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-03 22:28:23 -0700

    Update Home.js

--- dungeron_a/components/Home.js
+++ dungeron_a/components/Home.js
@@ -8,12 +8,12 @@ const Home = () => {
     {id:2, name: "throne", description: "King", width: 9, length:7 },
     {id:3, name: "guard", description: "Guards", width: 8, length:6 }
   ]
-  const selectRoom = (roomId) => {
-    setCurrent(<Room goHome={goHome} />);
-    //alert("select room"+roomId)
-  }
   const goHome = () => {
     setCurrent(<All rooms={roomData} selectRoom={selectRoom} />)
+  }
+  const selectRoom = (roomId) => {
+    setCurrent(<Room goHome={goHome()} />);
+    //alert("select room"+roomId)
   }
   const [current, setCurrent] = useState(<All rooms={roomData} selectRoom={selectRoom} />);
 
