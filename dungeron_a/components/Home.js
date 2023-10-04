ad8370ff6279729f5813238bca40831e02f3083c
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-03 22:13:42 -0700

    Update Home.js

--- dungeron_a/components/Home.js
+++ dungeron_a/components/Home.js
@@ -4,13 +4,13 @@ import Room from "./Room.js";
 
 const Home = () => {
   const roomData = [
-    {name: "entry", description: "enter", width: 5, length:5 },
-    {name: "throne", description: "King", width: 9, length:7 },
-    {name: "guard", description: "Guards", width: 8, length:6 }
+    {id:1, name: "entry", description: "enter", width: 5, length:5 },
+    {id:2, name: "throne", description: "King", width: 9, length:7 },
+    {id:3, name: "guard", description: "Guards", width: 8, length:6 }
   ]
-  const selectRoom = () => {
-    setCurrent(<Room />)
-    alert("select room")
+  const selectRoom = (roomId) => {
+    setCurrent(<Room goHome={setCurrent(<All rooms={roomData} selectRoom={selectRoom} />)});
+    //alert("select room"+roomId)
   }
   const [current, setCurrent] = useState(<All rooms={roomData} selectRoom={selectRoom} />);
 
