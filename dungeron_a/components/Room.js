03128ba2aa7651d3ec23cd5396d508cc16a91d61
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-04 21:41:50 -0700

    Update Room.js

--- dungeron_a/components/Room.js
+++ dungeron_a/components/Room.js
@@ -2,7 +2,7 @@ const Room = (goHome) => {
   console.log(`gh ${goHome.values}`)
   return(
     <div>
-      <button onClick={goHome}>click</button>
+      <button onClick={() => goHome()}>click</button>
       <h3>Room</h3>
     </div>
   )
