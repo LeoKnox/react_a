6a0105c65dcecf6a1c8a5c3f8782caca025284f8
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-06 20:33:31 -0700

    Create dungeonData.js

---
+++ dungeon_a/components/dungeonData.js
@@ -0,0 +1,9 @@
+let dungeonData = [
+  {id: 1, name:"entry", Description:"Entrance", width:5, length:5},
+  {id: 2, name:"throne", Description:"Throne Room", width:6, length:7},
+  {id: 3, name:"guard", Description:"Guard Room", width:9, length:8}
+]
+
+export function allRooms() {
+  return (dungeonData)
+}
