d2a18ba421ad0f540c91f5b1c841d86e1987a330
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-06 20:55:23 -0700

    Update Home.js

--- dungeon_a/components/Home.js
+++ dungeon_a/components/Home.js
@@ -5,9 +5,22 @@ const Home = () => {
   return (
     <div>
       <h1>Dungeon</h1>
+      <table>
+      <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Width</th>
+        <th>Length</th>
+      </tr>
       {dungeon.map(room => (
-        <td>{room.name}</td>
+        <tr>
+          <td>{room.name}</td>
+          <td>{room.description}</td>
+          <td>{room.width}</td>
+          <td>{room.length}</td>
+        </tr>
       ))}
+      </table>
     </div>
   )
 }
