6d5c5c0190ae17129da6ae8a2b6a1bf3e9e2f961
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-06 21:25:11 -0700

    Update Home.js

--- dungeon_a/components/Home.js
+++ dungeon_a/components/Home.js
@@ -1,33 +1,15 @@
-import {allRooms} from './dungeonData.js';
+import "./styles.css";
+import Home from "./components/Home.js";
+import { useState } from "React";
 
-const Home = () => {
-  let dungeon = allRooms();
-  function showRoom(roomId = 1) {
-    //alert("red");
-    console.log("red "+roomId)
+export default function App() {
+  const [currPage, setCurrPage] = useState("home");
+  const pages = {
+    'home': <Home />
   }
   return (
-    <div>
-      <h1>Dungeon</h1>
-      <table>
-      <tr>
-        <th>Name</th>
-        <th>Description</th>
-        <th>Width</th>
-        <th>Length</th>
-      </tr>
-      {dungeon.map(room => (
-        <tr>
-          <td>{room.name}</td>
-          <td>{room.description}</td>
-          <td>{room.width}</td>
-          <td>{room.length}</td>
-          <td><button onClick={() => showRoom(room.id)}>Show</button></td>
-        </tr>
-      ))}
-      </table>
+    <div className="App">
+      {pages.currPage}
     </div>
-  )
+  );
 }
-
-export default Home;
