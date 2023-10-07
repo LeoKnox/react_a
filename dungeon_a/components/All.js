697e3424665f01778aa45ceb9b277c0b704ca85e
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-06 21:13:32 -0700

    Update All.js

--- dungeon_a/components/All.js
+++ dungeon_a/components/All.js
@@ -1,33 +1,11 @@
-import {allRooms} from './dungeonData.js';
+import {All} from './All.js';
 
-const All = () => {
-  let dungeon = allRooms();
-  function showRoom(roomId = 1) {
-    //alert("red");
-    console.log("red "+roomId)
-  }
+const Home = () => {
   return (
     <div>
-      <h1>Dungeon</h1>
-      <table>
-      <tr>
-        <th>Name</th>
-        <th>Description</th>
-        <th>Width</th>
-        <th>Length</th>
-      </tr>
-      {dungeon.map(room => (
-        <tr>
-          <td>{room.name}</td>
-          <td>{room.description}</td>
-          <td>{room.width}</td>
-          <td>{room.length}</td>
-          <td><button onClick={() => showRoom(room.id)}>Show</button></td>
-        </tr>
-      ))}
-      </table>
+      <All />
     </div>
   )
 }
 
-export default All;
+export default Home;
