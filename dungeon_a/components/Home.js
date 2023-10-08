82c79f914441cd23916aebe99b1f786f08ce4ca3
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-07 21:54:50 -0700

    Update Home.js

--- dungeon_a/components/Home.js
+++ dungeon_a/components/Home.js
@@ -1,15 +1,18 @@
 
-import Home from "./Home.js";
-import { useState } from "react";
+import All from "./All.js";
+import {Room} from "./Room.js";
+//import { useState } from "react";
 
 export default function App() {
+  /*
   const [currPage, setCurrPage] = useState("home");
   const pages = {
     'home': <Home />
   }
+  */
   return (
     <div className="App">
-      {pages.currPage}
+      <Room />
     </div>
   );
 }
