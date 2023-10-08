7462d9f205809a6e2601e9e058144392ff7990e3
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-07 22:38:36 -0700

    Update Home.js

--- dungeon_a/components/Home.js
+++ dungeon_a/components/Home.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import Room from "./Room.js";
+import All from "./All.js";
 
 const Home = () => {
   const [currPage, setCurrPage] = useState("room");
+  function changePage() {
+    setCurrPage("all");
+  }
   const thisPage = {
-    room: <Room />
+    room: <Room changePage={changePage} />,
+    all: <All />
   };
 
   return (thisPage[currPage]);
