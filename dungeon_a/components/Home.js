3ff27dda1ac0fcb9adf66fce5ceeb35e55b099b8
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-08 21:46:03 -0700

    Update Home.js

--- dungeon_a/components/Home.js
+++ dungeon_a/components/Home.js
@@ -4,8 +4,8 @@ import All from "./All.js";
 
 const Home = () => {
   const [currPage, setCurrPage] = useState("room");
-  function changePage(page="room") {
-    console.log("change page "+page)
+  function changePage(event, page="room") {
+    console.log("change page "+event.target.value)
     setCurrPage("all");
   }
   const thisPage = {
