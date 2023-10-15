13ab692415450819ffa3e926ada13b2c507480c4
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-14 21:35:44 -0700

    Update Home.js

--- dungeon_a/components/Home.js
+++ dungeon_a/components/Home.js
@@ -1,30 +1,32 @@
-import { useState } from "react";
-import Room from "./Room.js";
-import All from "./All.js";
+import {allRooms} from './dungeonData.js';
 
-const Home = () => {
-  const [currPage, setCurrPage] = useState("room");
-  const [currRoom, setCurrRoom] = useState();
-  const changePage = (event) => {
-    console.log("change page "+event.target.value)
-    setCurrPage(event.target.value);
-  }
-  
-  function setRoom(event) {
-    setCurrRoom(event.target.value)
-  }
-  const thisPage = {
-    room: <Room />,
-    all: <All setRoom = {() => setCurrRoom()} />
-  };
-
+const All = (setCurrRoom) => {
+  let rooms = allRooms();
   return (
     <div>
-      <h1>Title {currRoom}</h1>
-      <button onClick={changePage} value="all">All</button>
-      <button onClick={changePage} value="room">Room</button>
-      {thisPage[currPage]}
+      <h3>All</h3>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Width</th>
+          <th>Length</th>
+          <th>Action</th>
+        </tr>
+      {rooms.map(room => (
+        <tr>
+          <td>{room.name}</td>
+          <td>{room.description}</td>
+          <td>{room.width}</td>
+          <td>{room.length}</td>
+          <td>
+            <button value="red" onClick={setCurrRoom}>Room</button>
+          </td>
+        </tr>
+      ))}
+      </table>
     </div>
-  )};
+  );
+};
 
-export default Home;
+export default All;
