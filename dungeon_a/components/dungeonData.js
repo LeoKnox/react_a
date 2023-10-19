34cd31d06288c5600e4631ca885374a0d7433364
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-18 21:25:20 -0700

    Update dungeonData.js

--- dungeon_a/components/dungeonData.js
+++ dungeon_a/components/dungeonData.js
@@ -1,9 +1,13 @@
 let dungeonData = [
-  {id: 1, name:"entry", Description:"Entrance", width:5, length:5},
-  {id: 2, name:"throne", Description:"Throne Room", width:6, length:7},
-  {id: 3, name:"guard", Description:"Guard Room", width:9, length:8}
+  {id: 1, name:"entry", description:"Entrance", width:5, length:5},
+  {id: 2, name:"throne", description:"Throne Room", width:6, length:7},
+  {id: 3, name:"guard", description:"Guard Room", width:9, length:8}
 ]
 
 export function allRooms() {
   return (dungeonData)
+}
+
+export function roomData(id) {
+  return(dungeonData[id-1])
 }
