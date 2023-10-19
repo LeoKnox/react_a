a3ce351ff053eed4e1867fecd129868f0d104e2f
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-18 21:30:54 -0700

    Update Room.js

--- dungeon_a/components/Room.js
+++ dungeon_a/components/Room.js
@@ -2,10 +2,10 @@ import {roomData } from './dungeonData.js'
 
 const Room = ({roomNum=0}) => {
   let room = roomData(roomNum)
-  console.log("room " + {room})
+  console.log("room " + {room[name]})
   return (
     <div>
-      <h3>Room {roomNum}</h3>
+      <h3>Room {room}</h3>
     </div>
   );
 };
