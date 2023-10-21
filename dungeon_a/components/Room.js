7c31585111e34b484dbf17b091a9f81e159184d8
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-20 22:28:07 -0700

    Update Room.js

--- dungeon_a/components/Room.js
+++ dungeon_a/components/Room.js
@@ -8,9 +8,17 @@ const Room = ({roomNum=0}) => {
       <p><b>Description</b></p>
       <p>{room.description}</p>
       <p>
+        <label className="roomInfo">
         Width x Length: &#160;
+        </label>
+        <label className="roomInfo">
+        <label className="roomInfo">  
         {room.width}&#160; 
+        </label>
+        <label className="roomInfo">
         {room.length}
+        </label>
+        </label>
       </p>
     </div>
   );
