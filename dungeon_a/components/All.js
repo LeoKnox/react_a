5fd5b9c23e66a5418a086d6de1e4bba8502792f0
LeoKnox <LeoKnox@users.noreply.github.com>
2023-10-31 23:04:53 -0700

    Update All.js

--- dungeon_a/components/All.js
+++ dungeon_a/components/All.js
@@ -51,13 +51,15 @@ const All = ({setRoom}) => {
         </tr>
       ))}
       <tr>
+          <form onSubmit={submitRoom}>
           <td><input type="text" name="name" value={name} onChange={e => setName(e.target.value)} /></td>
           <td><input type="text" name="description" value={description} onChange={e => setDescription(e.target.value)} /></td>
           <td><input type="number" name="width" value={width} onChange={e => setWidth(e.target.value)} /></td>
           <td><input type="number" name="length" value={length} onChange={e => setLength(e.target.value)} /></td>
           <td>
-            <buttton onClick={submitRoom}>Create</buttton>
+            <button type="submit">create</button>
           </td>
+          </form>
       </tr>
       </table>
     </div>
