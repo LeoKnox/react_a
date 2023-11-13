ef5735a41c7a7036a462198d8ce361f2e2b456dd
LeoKnox <LeoKnox@users.noreply.github.com>
2023-11-12 22:24:38 -0800

    Update dungeonData.js

--- dungeon_a/components/dungeonData.js
+++ dungeon_a/components/dungeonData.js
@@ -1,10 +1,12 @@
-import { useContext, useCallback, useReducer } from 'react'
+import { useContext, createContext, useCallback, useReducer } from 'react'
 
 let dungeonData = [
   {id: 1, name:"entry", description:"Entrance", width:5, length:5},
   {id: 2, name:"throne", description:"Throne Room", width:6, length:7},
   {id: 3, name:"guard", description:"Guard Room", width:9, length:8}
 ]
+
+export const redContext = createContext("red")
 
 export function allRooms() {
   return (dungeonData)
