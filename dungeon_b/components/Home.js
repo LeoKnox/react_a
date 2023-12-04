534b7680934997da3b8d670eaab9448d76b36994
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-03 21:28:29 -0800

    Create Home.js

---
+++ dungeon_b/components/Home.js
@@ -0,0 +1,20 @@
+import react from "react";
+import Rooms from "./components/Rooms";
+
+function Home() {
+  return;
+  <>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Height</th>
+          <th>Width</th>
+        </tr>
+      </thead>
+    </table>
+  </>;
+}
+
+export default Home;
