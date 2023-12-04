f2f689e3ac47cc182c1983c7b7b8f5699e3ec5e1
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-03 21:05:49 -0800

    Update Rooms.js

--- dungeon_b/components/Rooms.js
+++ dungeon_b/components/Rooms.js
@@ -1,7 +1,7 @@
-const rooms = [
+const Rooms = [
   {id:1, name:"entry", description:"entrance", length:5, width:5},
   {id:2, name:"guard", description:"guard room", length:7, width:8},
   {id:3, name:"treasure", description:"treasure room", length:8, width:6}
   ]
 
-export default rooms;
+export default Rooms;
