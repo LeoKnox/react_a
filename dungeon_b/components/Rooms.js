585d974f97dc2d59a5c313d9189ed1fc2c1848f7
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-08 22:13:59 -0800

    Update Rooms.js

--- dungeon_b/components/Rooms.js
+++ dungeon_b/components/Rooms.js
@@ -1,7 +1,17 @@
 const Rooms = [
-  {id:1, name:"entry", description:"entrance", length:5, width:5},
-  {id:2, name:"guard", description:"guard room", length:7, width:8},
-  {id:3, name:"treasure", description:"treasure room", length:8, width:6}
-  ]
+  { id: 1, name: "entry", description: "entrance", length: 5, width: 5 },
+  { id: 2, name: "guard", description: "guard room", length: 7, width: 8 },
+  {
+    id: 3,
+    name: "treasure",
+    description: "treasure room",
+    length: 8,
+    width: 6,
+  },
+];
 
-export default Rooms;
+function deleteRoom(id=0) {
+  console.log("red")
+}
+
+export default Rooms, {deleteRoom};
