c26c5f92e8977150d2ad7357a678e4871d3d862b
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-11 22:08:46 -0800

    Create roomslist.js

---
+++ dungeon_c/components/roomslist.js
@@ -0,0 +1,7 @@
+const roomslist = [
+  { id: 1, name: "Entry", description: "entrance", length: 5, width: 5 },
+  { id: 2, name: "Guard", description: "guard room", length: 7, width: 8 },
+  { id: 3, name: "Throne", description: "throne room", length: 6, width: 7 },
+]
+
+export default roomslist;
