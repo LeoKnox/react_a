bba69733b4f5b37bae79db497b078d7cb07d8d20
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-11 22:25:02 -0800

    Update Home.js

--- dungeon_c/components/Home.js
+++ dungeon_c/components/Home.js
@@ -1,6 +1,9 @@
 import roomslist from "./roomslist";
 
 function Home() {
+  function deleteRoom(id) {
+    alert(id);
+  }
   return (
     <div className="home">
       <h1>Home</h1>
@@ -8,6 +11,7 @@ function Home() {
         return (
           <p>
             {rooms.name}:{rooms.description}:{rooms.length}x{rooms.width}
+            <button onClick={(e) => deleteRoom(rooms.id)}>delete</button>
           </p>
         );
       })}
