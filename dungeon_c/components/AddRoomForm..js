48f4baa39b4071f089e00867a00d209a934bd059
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-15 22:20:41 -0800

    Update AddRoomForm..js

--- dungeon_c/components/AddRoomForm..js
+++ dungeon_c/components/AddRoomForm..js
@@ -13,9 +13,14 @@ const AddRoomForm = (props) => {
     <form>
       <label>Name</label>
       <input type="text" name="name" value={buildRoom.name} />
+      <label>Description</label>
       <input type="text" name="description" value={buildRoom.description} />
+      <br />
+      <label>Width</label>
       <input type="number" name="width" value={buildRoom.width} />
+      <label>Length</label>
       <input type="number" name="length" value={buildRoom.length} />
+      <br />
       <button type="submit">Create</button>
     </form>
   );
