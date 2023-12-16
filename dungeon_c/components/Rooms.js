f32a1d1a47c1d1d6b5cdae13d5c652fb7963b316
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-15 21:20:41 -0800

    Create Rooms.js

---
+++ dungeon_c/components/Rooms.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Rooms = props => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Width</th>
+                    <th>Height</th>
+                </tr>
+            </thead>
+            <tbody>
+                { props.rooms.length > 0 ? (
+                {props.rooms.map(room => {
+                    const {id, name, description, width, height}
+                    return (
+                        <tr>
+                            <td>{name}</td>
+                            <td>{description}</td>
+                            <td>{width}</td>
+                            <td>{height}</td>
+                            <td>
+                                <button>Delete</button>
+                                <button>Edit</button>
+                            </td>
+                        </tr>
+                    )
+                })}) : (<h3>No Rooms</h3>)}
+            </tbody>
+        </table>
+)
+}
