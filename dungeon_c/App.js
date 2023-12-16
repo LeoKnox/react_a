e3b1cf3fa5cdfed95e6cf8adb8503562b8d85ca8
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-15 21:25:58 -0800

    Update App.js

--- dungeon_c/App.js
+++ dungeon_c/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
 import Home from "./components/Home.js";
+import roomsList from "./components/roomslist.js";
+import Rooms from "./componpents/Rooms.js";
 
 export default function App() {
+  const [currRooms, setCurrRooms] = useState(roomsList);
   return (
     <div className="App">
-      <Home />
+      <Rooms rooms={currRooms} />
     </div>
   );
 }
