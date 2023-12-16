e359f5e0b3f93457fac9c2f72a823d872c78db5d
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-15 22:11:06 -0800

    Update App.js

--- dungeon_c/App.js
+++ dungeon_c/App.js
@@ -3,12 +3,18 @@ import { useState } from "react";
 import Home from "./components/Home.js";
 import roomsList from "./components/roomslist.js";
 import Rooms from "./components/Rooms.js";
+import AddRoomForm from "./components/AddRoomForm.js";
 
 export default function App() {
   const [currRooms, setCurrRooms] = useState(roomsList);
+  const addRoom = (room) => {
+    room.id = currRooms.length + 1;
+    setCurrRooms([...currRooms, room]);
+  };
   return (
     <div className="App">
       <h1>Dungeon Room</h1>
+      <AddRoomForm addRoom={addRoom} />
       <Rooms rooms={currRooms} />
     </div>
   );
