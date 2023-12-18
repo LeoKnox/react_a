0731215956947280c01fa85f8fe87f018cc9e3d3
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-17 20:59:27 -0800

    Update AddRoomForm..js

--- dungeon_c/components/AddRoomForm..js
+++ dungeon_c/components/AddRoomForm..js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const AddRoomForm = (props) => {
   const buildRoom = {
     id: null,
-    name: "zzz",
+    name: "",
     description: "",
     width: 0,
     length: 0,
@@ -19,12 +19,27 @@ const AddRoomForm = (props) => {
       <label>Name</label>
       <input type="text" name="name" onClick={handleChange} value={room.name} />
       <label>Description</label>
-      <input type="text" name="description" value={room.description} />
+      <input
+        type="text"
+        name="description"
+        onClick={handleChange}
+        value={room.description}
+      />
       <br />
       <label>Width</label>
-      <input type="number" name="width" value={buildRoom.width} />
+      <input
+        type="number"
+        name="width"
+        onClick={handleChange}
+        value={buildRoom.width}
+      />
       <label>Length</label>
-      <input type="number" name="length" value={buildRoom.length} />
+      <input
+        type="number"
+        name="length"
+        onClick={handleChange}
+        value={buildRoom.length}
+      />
       <br />
       <button type="submit">Create</button>
     </form>
