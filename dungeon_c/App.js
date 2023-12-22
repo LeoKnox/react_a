70f570276cede6b95f5a827a4ce7f63b4df29673
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-21 18:52:48 -0800

    Update App.js

--- dungeon_c/App.js
+++ dungeon_c/App.js
@@ -1,24 +1,68 @@
-import "./styles.css";
-import { useState } from "react";
-import Home from "./components/Home.js";
-import roomsList from "./components/roomslist.js";
-import Rooms from "./components/Rooms.js";
-import AddRoomForm from "./components/AddRoomForm.js";
+import React, { useState } from "react";
 
-export default function App() {
-  const [currRooms, setCurrRooms] = useState(roomsList);
-  const addRoom =
-    ((room) => {
-      alert("red");
-      room.id = currRooms.length + 1;
-      setCurrRooms([...currRooms, room]);
-    },
-    []);
+const AddRoomForm = (addRoom) => {
+  const buildRoom = {
+    id: null,
+    name: "",
+    description: "",
+    width: 0,
+    length: 0,
+  };
+  //console.log("props" + JSON.stringify(addRoom));
+  const [room, setRoom] = useState(buildRoom);
+  const submitRoom = (e) => {
+    //e.preventdefault();
+    let newRoom = {
+      name: e.target.name.value,
+      description: e.target.description.value,
+      width: e.target.width.value,
+      length: e.target.length.value,
+    };
+    //setRoom(room.push(newRoom));
+    addRoom(newRoom);
+    console.log(JSON.stringify(room));
+  };
+  const increaseSize = (e) => {
+    const { name, value } = e.target;
+    let newValue = parseInt(value);
+    console.log(name + ":" + newValue);
+    setRoom({ ...room, [name]: newValue });
+  };
+  const handleChange = (e) => {
+    const { name, evalue } = e.target;
+    //console.log(name + ":" + evalue);
+    setRoom({ ...room, [name]: evalue });
+  };
   return (
-    <div className="App">
-      <h1>Dungeon Room</h1>
-      <AddRoomForm addRoom={addRoom} />
-      <Rooms rooms={currRooms} />
-    </div>
+    <form onSubmit={submitRoom}>
+      <label>Name</label>
+      <input type="text" name="name" onClick={handleChange} value={room.name} />
+      <label>Description</label>
+      <input
+        type="text"
+        name="description"
+        onClick={handleChange}
+        value={room.description}
+      />
+      <br />
+      <label>Width</label>
+      <input
+        type="number"
+        name="width"
+        onChange={increaseSize}
+        value={room.width}
+      />
+      <label>Length</label>
+      <input
+        type="number"
+        name="length"
+        onChange={increaseSize}
+        value={room.length}
+      />
+      <br />
+      <input type="submit" value="Create" />
+    </form>
   );
-}
+};
+
+export default AddRoomForm;
