1ecb390ebdda759d7f4c4be43bfa2923282c182f
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-21 18:49:57 -0800

    Update App.js

--- dungeon_c/App.js
+++ dungeon_c/App.js
@@ -7,11 +7,13 @@ import AddRoomForm from "./components/AddRoomForm.js";
 
 export default function App() {
   const [currRooms, setCurrRooms] = useState(roomsList);
-  const addRoom = (room) => {
-    alert("red");
-    room.id = currRooms.length + 1;
-    setCurrRooms([...currRooms, room]);
-  };
+  const addRoom =
+    ((room) => {
+      alert("red");
+      room.id = currRooms.length + 1;
+      setCurrRooms([...currRooms, room]);
+    },
+    []);
   return (
     <div className="App">
       <h1>Dungeon Room</h1>
