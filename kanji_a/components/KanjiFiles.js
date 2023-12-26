4df3f67bc6b3e6d7a4b7c2a9c3b005fea745dbe2
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-25 18:59:33 -0800

    Create KanjiFiles.js

---
+++ kanji_a/components/KanjiFiles.js
@@ -0,0 +1,17 @@
+export const KanjiList = [
+  {
+    id: 1,
+    word: "akai",
+    translation: "red",
+  },
+  {
+    id: 2,
+    word: "aoi",
+    translation: "blue",
+  },
+  {
+    id: 1,
+    word: "midori",
+    translation: "green",
+  },
+];
