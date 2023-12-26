233d22d33c29e68106b54e22d2e5e3b2356981ed
LeoKnox <LeoKnox@users.noreply.github.com>
2023-12-25 19:16:08 -0800

    Update App.js

--- kanji_a/App.js
+++ kanji_a/App.js
@@ -1,6 +1,8 @@
 import "./styles.css";
+import { KanjiList } from "./components/KanjiList.js";
 
 export default function App() {
+  console.log(KanjiList[1]);
   return (
     <div className="App">
       <h1>Kanjis</h1>
