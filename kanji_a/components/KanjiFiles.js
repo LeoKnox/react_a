d076c63af6860ccc1798614db6b556dc897a5f22
LeoKnox <LeoKnox@users.noreply.github.com>
2024-01-03 21:42:36 -0800

    Update KanjiFiles.js

--- kanji_a/components/KanjiFiles.js
+++ kanji_a/components/KanjiFiles.js
@@ -1,16 +1,19 @@
 export const KanjiList = [
   {
     id: 1,
+    kanji: "赤",
     word: "akai",
     translation: "red",
   },
   {
     id: 2,
+    kanji: "青",
     word: "aoi",
     translation: "blue",
   },
   {
     id: 3,
+    kanji: "みどり",
     word: "midori",
     translation: "green",
   },
