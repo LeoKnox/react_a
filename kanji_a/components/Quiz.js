a3398b2280632b5b183708757d7fa59a79ec8fb6
LeoKnox <LeoKnox@users.noreply.github.com>
2024-01-04 17:55:58 -0800

    Update Quiz.js

--- kanji_a/components/Quiz.js
+++ kanji_a/components/Quiz.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 export const Quiz = ({ myKanjis }) => {
   console.log(myKanjis);
+  const [quizKanji, setQuizKanji] = useState(myKanjis);
   return (
     <div>
       <h1>Quizes</h1>
+      {quizKanji.map((kanji) => {
+        <div>{kanji.kanji}</div>;
+      })}
     </div>
   );
 };
