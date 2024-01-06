8d0f03a7288a1e301c6921e36a79e5471978df59
LeoKnox <LeoKnox@users.noreply.github.com>
2024-01-05 22:43:11 -0800

    Update Quiz.js

--- kanji_a/components/Quiz.js
+++ kanji_a/components/Quiz.js
@@ -1,23 +1,136 @@
-import { useEffect, useState } from "react";
+import "./styles.css";
+import { useEffect, useState, useRef } from "react";
+import { KanjiList } from "./components/KanjiList.js";
+import { Quiz } from "./components/Quiz.js";
 
-export const Quiz = ({ myKanjis }) => {
-  console.log(myKanjis);
-  const [quizKanji, setQuizKanji] = useState();
+export default function App() {
+  const [myKanji, setMyKanji] = useState([]);
+  const [word, setWord] = useState("");
+  const [translation, setTranslation] = useState("");
+  const [id, setId] = useState(0);
+  const [isUpdate, setIsUpdate] = useState(false);
   useEffect(() => {
-    setQuizKanji(myKanjis);
-  });
-  console.log("xx");
-  console.log(quizKanji);
+    setMyKanji(KanjiList);
+  }, []);
+  const handleSave = (e) => {
+    let error = "";
+    if (word === "") {
+      error += "word needed";
+    }
+    if (translation === "") {
+      error += "translation needed";
+    }
+    if (error === "") {
+      e.preventDefault();
+      newKanji = {
+        id: myKanji.length + 1,
+        word: word,
+        translation: translation,
+      };
+      const kt = [...myKanji, newKanji];
+      setMyKanji(kt);
+    } else {
+      alert(error);
+    }
+  };
+  const handleUpdate = () => {
+    const index = myKanji
+      .map((kanji) => {
+        return kanji.id;
+      })
+      .indexOf(id);
+    const kt = [...myKanji];
+    kt[index].word = word;
+    kt[index].translation = translation;
+    setMyKanji(kt);
+    handleClear();
+  };
+  const handleClear = () => {
+    setIsUpdate(false);
+    setId(0);
+    setWord("");
+    setTranslation("");
+  };
+  const handleEdit = (id) => {
+    const kd = myKanji.filter((item) => item.id === id);
+    if (kd !== undefined) {
+      setIsUpdate(true);
+      setId(kd[0].id);
+      setWord(kd[0].word);
+      setTranslation(kd[0].translation);
+    } else {
+      alert("ni");
+    }
+  };
+  const handleDelete = (id) => {
+    if (id > 0) {
+      if (window.confirm("Are you sure?")) {
+        const kd = myKanji.filter((item) => item.id !== id);
+        setMyKanji(kd);
+        handleClear();
+      }
+    }
+  };
+
   return (
-    <div>
-      <h1>Quizes</h1>
-      {quizKanji.map((kanji) => {
-        return (
-          <div>
-            <p>{kanji.kanji}</p>
-          </div>
-        );
-      })}
+    <div className="App">
+      <h1 className="mainHeading">Kanjis</h1>
+      <ul className="addKanji">
+        <li>
+          <label>Word:</label>
+          <input
+            type="text"
+            placeholder="enter word"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+          />
+        </li>
+        <li>
+          <label>Translation:</label>
+          <input
+            type="text"
+            value={translation}
+            placeholder="enter translation"
+            onChange={(e) => setTranslation(e.target.value)}
+          />
+        </li>
+        <li>
+          {!isUpdate ? (
+            <button onClick={(e) => handleSave(e)}>Save</button>
+          ) : (
+            <button onClick={handleUpdate}>Update</button>
+          )}
+          <button onClick={handleClear}>Clear</button>
+        </li>
+      </ul>
+      <br />
+      <table className="kanjis">
+        <thead className="kanjisheader">
+          <tr>
+            <th>Kanji</th>
+            <th>Word</th>
+            <th>Translation</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {myKanji.map((kanjis, index) => {
+            return (
+              <tr key={index} className="kanjisdata">
+                <td claasName="kanjiStyle">{kanjis.kanji}</td>
+                <td>{kanjis.word}</td>
+                <td>{kanjis.translation}</td>
+                <td>
+                  <button onClick={() => handleEdit(kanjis.id)}>edit</button>
+                  <button onClick={() => handleDelete(kanjis.id)}>
+                    delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
-};
+}
