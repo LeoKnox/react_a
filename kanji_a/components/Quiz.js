f30d93d42dec795b86860a00dae69fb05ce70d8c
LeoKnox <LeoKnox@users.noreply.github.com>
2024-01-13 15:16:24 -0800

    Update Quiz.js

--- kanji_a/components/Quiz.js
+++ kanji_a/components/Quiz.js
@@ -22,8 +22,8 @@ export const Quiz = ({ myKanji }) => {
   useEffect(() => {
     console.log("aaa");
     let y = findKanji();
-    console.log(typeof y);
-    setAnswer(y[y[Math.floor(Math.random() * y.length())]]["tanslation"]);
+    console.log(y.length);
+    setAnswer(y[y[Math.floor(Math.random() * y.length)]]["tanslation"]);
     console.log("bbb" + y);
     setQuizKanji(y);
     console.log("+++" + quizKanji);
