1fd57048feb7e1ae65f4be4dc6324589a6e61bdf
LeoKnox <LeoKnox@users.noreply.github.com>
2024-01-16 14:54:41 -0800

    Update Quiz.js

--- kanji_a/components/Quiz.js
+++ kanji_a/components/Quiz.js
@@ -41,7 +41,7 @@ export const Quiz = ({ myKanji }) => {
   return (
     <div>
       <h1>Quizes</h1>
-      <h3>{myKanji[quizKanji[answer]]--}</h3>
+      <h3>{myKanji[quizKanji[answer - 1]["translation"]]--}</h3>
       {quizKanji.map((kanji) => {
         return (
           <div>
