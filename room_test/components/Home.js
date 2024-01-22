e4fd72d3e1175e3e4d9772f789ead2c56205129c
LeoKnox <LeoKnox@users.noreply.github.com>
2024-01-21 17:20:19 -0800

    Update Home.js

--- room_test/components/Home.js
+++ room_test/components/Home.js
@@ -1,4 +1,7 @@
-export default Home = () => {
+import { memo } from "react";
+
+export default Home = ({ myRooms, addRoom }) => {
+  console.log(JSON.stringify(myRooms));
   return (
     <>
       <h1>Rooms</h1>
