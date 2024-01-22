3fdcf79610d1af75a1ee554caffdb76d5e6d9832
LeoKnox <LeoKnox@users.noreply.github.com>
2024-01-21 17:19:41 -0800

    Update App.js

--- room_test/App.js
+++ room_test/App.js
@@ -1,9 +1,19 @@
-import { memo } from "react";
+import "./styles.css";
+import "./components/Home.js";
+import { useState } from "react";
 
-export default Home = ({ myRooms, addRoom }) => {
+export default function App() {
+  const [myRooms, setMyRooms] = useState([
+    { name: "entry", width: 5, length: 5 },
+    { name: "store", width: 8, length: 6 },
+    { name: "guard", width: 6, length: 7 },
+  ]);
+  const addRoom = () => {
+    setMyRooms = (room) => [...room, { name: "new room", width: 1, length: 1 }];
+  };
   return (
-    <>
-      <h1>Rooms</h1>
-    </>
+    <div className="App">
+      <Home myRooms={myRooms} addRoom={addRoom} />
+    </div>
   );
-};
+}
