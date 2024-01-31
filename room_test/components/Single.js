861eb996a1e1ed2ebcd39cd21465bd136e97efab
LeoKnox <LeoKnox@users.noreply.github.com>
2024-01-31 11:52:38 -0800

    Update Single.js

--- room_test/components/Single.js
+++ room_test/components/Single.js
@@ -7,7 +7,13 @@ export default Single = ({ roomId, room, setRoomId }) => {
       <h3>Single Room {roomId} </h3>
       <button onClick={() => setIsEdit(true)}>Edit</button>
       <button onClick={() => setRoomId(-1)}>Home</button>
-      <p>{room["name"]}</p>
+      {isEdit ? (
+        <p>
+          <input type="text" value="newName" />
+        </p>
+      ) : (
+        <p>{room["name"]}</p>
+      )}
       <p>
         Width:{room["width"]} Length:{room["length"]}
       </p>
