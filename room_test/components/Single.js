1843213fc58ffd690c835777b1c046efce80248c
LeoKnox <LeoKnox@users.noreply.github.com>
2024-01-31 11:47:15 -0800

    Update Single.js

--- room_test/components/Single.js
+++ room_test/components/Single.js
@@ -1,11 +1,13 @@
+import { useState } from "react";
+
 export default Single = ({ roomId, room, setRoomId }) => {
+  const [isEdit, setIsEdit] = useState(false);
   return (
     <>
       <h3>Single Room {roomId} </h3>
+      <button onClick={() => setIsEdit(true)}>Edit</button>
       <button onClick={() => setRoomId(-1)}>Home</button>
       <p>{room["name"]}</p>
-      <p>Description:</p>
-      <p>{room["description"]}</p>
       <p>
         Width:{room["width"]} Length:{room["length"]}
       </p>
