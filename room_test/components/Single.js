747b02afb35916fef02fd916f0db31901c39cbd0
LeoKnox <LeoKnox@users.noreply.github.com>
2024-01-31 11:40:30 -0800

    Update Single.js

--- room_test/components/Single.js
+++ room_test/components/Single.js
@@ -4,6 +4,11 @@ export default Single = ({ roomId, room, setRoomId }) => {
       <h3>Single Room {roomId} </h3>
       <button onClick={() => setRoomId(-1)}>Home</button>
       <p>{room["name"]}</p>
+      <p>Description:</p>
+      <p>{room["description"]}</p>
+      <p>
+        Width:{room["width"]} Length:{room["length"]}
+      </p>
     </>
   );
 };
