d2bf125e668b400e37d8a8dd10a0c0681992c4de
LeoKnox <LeoKnox@users.noreply.github.com>
2024-02-01 11:52:33 -0800

    Update App.js

--- room_test/App.js
+++ room_test/App.js
@@ -1,39 +1,56 @@
+import "./styles.css";
+import "./components/Home.js";
+import "./components/Single.js";
 import { useState } from "react";
 
-export default Single = ({ roomId, room, setRoomId, updateRoom }) => {
-  const [newRoom, setNewRoom] = useState(room);
-  const [isEdit, setIsEdit] = useState(false);
-  console.log(`New room ${JSON.stringify(newRoom)}`);
-  const changeValue = (e) => {
-    const { name, value } = e.target;
-    console.log(`name ${name}`);
-    setNewRoom((item) => ({
-      ...item,
-      ...item.newRoom,
-      [name]: value,
-    }));
+export default function App() {
+  const [roomId, setRoomId] = useState(-1);
+  const [myRooms, setMyRooms] = useState([
+    { id: 0, name: "entry", width: 5, length: 5 },
+    { id: 1, name: "store", width: 8, length: 6 },
+    { id: 2, name: "guard", width: 6, length: 7 },
+  ]);
+  const addRoom = (roomName) => {
+    console.log(`room name ${JSON.stringify(roomName)}`);
+    const newId = myRooms[myRooms.length - 1]["id"] + 1;
+    console.log(roomName);
+    roomName["id"] = newId;
+    setMyRooms((room) => [
+      ...room,
+      roomName,
+      /*{ id: newId, name: roomName, width: 1, length: 1 },*/
+    ]);
+  };
+  const deleteRoom = (id) => {
+    setMyRooms(myRooms.filter((room) => room["id"] !== id));
+    console.log(id);
+  };
+  const updateRoom = (updatedRoom) => {
+    console.log(`updated room ${JSON.stringify(updatedRoom)}`);
+    const tempRoom = myRooms.map((room) => {
+      if (roomId === room["id"]) {
+        room = updatedRoom;
+      }
+    });
+    console.log(JSON.stringify(tempRoom));
   };
   return (
-    <>
-      <h3>Single Room {roomId} </h3>
-      <button onClick={() => setRoomId(-1)}>Home</button>
-      {isEdit ? (
-        <p>
-          <input
-            type="text"
-            name="name"
-            value={newRoom["name"]}
-            onChange={changeValue}
-          />
-        </p>
+    <div className="App">
+      {roomId < 0 ? (
+        <Home
+          myRooms={myRooms}
+          addRoom={addRoom}
+          setRoomId={setRoomId}
+          deleteRoom={(id) => deleteRoom(id)}
+        />
       ) : (
-        <p>{room["name"]}</p>
+        <Single
+          roomId={roomId}
+          room={myRooms[roomId]}
+          setRoomId={setRoomId}
+          updateRoom={updateRoom}
+        />
       )}
-      <p>
-        Width:{room["width"]} Length:{room["length"]}
-      </p>
-      <button onClick={() => setIsEdit(true)}>Edit</button>
-      <button onClick={() => updateRoom(newRoom)}>Update</button>
-    </>
+    </div>
   );
-};
+}
