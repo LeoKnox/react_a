1c28e9c43a1e0730ba032bfcc06421f4ce1954e9
LeoKnox <LeoKnox@users.noreply.github.com>
2024-01-31 17:41:06 -0800

    Update Single.js

--- room_test/components/Single.js
+++ room_test/components/Single.js
@@ -4,6 +4,9 @@ export default Single = ({ roomId, room, setRoomId }) => {
   const [newRoom, setNewRoom] = useState(room);
   const [isEdit, setIsEdit] = useState(false);
   console.log(`New room ${JSON.stringify(newRoom)}`);
+  const changeValue = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <>
       <h3>Single Room {roomId} </h3>
@@ -11,7 +14,7 @@ export default Single = ({ roomId, room, setRoomId }) => {
       <button onClick={() => setRoomId(-1)}>Home</button>
       {isEdit ? (
         <p>
-          <input type="text" value={newRoom["name"]} onChange{(e) => setNewRoom(name: e.target.value)} />
+          <input type="text" value={newRoom["name"]} onChange={changeValue} />
         </p>
       ) : (
         <p>{room["name"]}</p>
