2b7940b8f0bb2048b1addd66d051049ad68d7bce
LeoKnox <LeoKnox@users.noreply.github.com>
2024-02-03 10:16:04 -0800

    Update DrawRoom.js

--- room_test/components/DrawRoom.js
+++ room_test/components/DrawRoom.js
@@ -6,9 +6,9 @@ export default DrawRoom = ({ width, length }) => {
     for (let y = 0; y < length + 2; y++) {
       for (let x = 0; x < width + 2; x++) {
         if (y === 0 || y === length + 1 || x === 0 || x === width + 1) {
-          newWidth.push(<td>*</td>);
+          newWidth.push(<td>&#9632;</td>);
         } else {
-          newWidth.push(<td>.</td>);
+          newWidth.push(<td>&#183;</td>);
         }
       }
       newLength.push(<tr>{newWidth}</tr>);
