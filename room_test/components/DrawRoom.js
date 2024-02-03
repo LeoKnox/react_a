39bd41720fdeb6878ed991dc9850a733c23f89f7
LeoKnox <LeoKnox@users.noreply.github.com>
2024-02-02 18:19:03 -0800

    Update DrawRoom.js

--- room_test/components/DrawRoom.js
+++ room_test/components/DrawRoom.js
@@ -1,16 +1,14 @@
 export default DrawRoom = ({ width, length }) => {
-    const renderRoom = () => {
-        return (<td>*</td>)
-    }
+  const renderRoom = () => {
+    return <td>*</td>;
+  };
   return (
     <div>
       <h3>Picture of Room</h3>
       <p>
         {width}:{length}
       </p>
-      <table>
-        {renderRoom}
-        </table>
+      <table>{renderRoom()}</table>
     </div>
   );
 };
