440cdec2d9ec3d03615d5573cd1deb2c8e1fcec6
LeoKnox <LeoKnox@users.noreply.github.com>
2024-02-06 16:37:53 -0800

    Update DrawRoom.js

--- room_test/components/DrawRoom.js
+++ room_test/components/DrawRoom.js
@@ -9,22 +9,6 @@ export default DrawRoom = ({ width, length }) => {
   const renderRoom = () => {
     console.log(`render room ${length} width ${width}`);
     let newWidth = [];
-    let newLength = [[":x"]];
-    console.log(`new width ${newWidth.length} new length ${newLength.length}`);
-    for (let y = 0; y < parseInt(length) + 2; y++) {
-      for (let x = 0; x < parseInt(width) + 2; x++) {
-        if (
-          y === 0 ||export default DrawRoom = ({ width, length }) => {
-  const tableStyle = {
-    color: "blue",
-    backgroundColor: "lightgray",
-    width: "30%",
-    marginLeft: "auto",
-    marginRight: "auto",
-  };
-  const renderRoom = () => {
-    console.log(`render room ${length} width ${width}`);
-    let newWidth = [];
     let newLength = [];
     console.log(`new width ${newWidth.length} new length ${newLength.length}`);
     for (let y = 0; y < parseInt(length) + 2; y++) {
@@ -35,46 +19,26 @@ export default DrawRoom = ({ width, length }) => {
           x === 0 ||
           x === parseInt(width) + 1
         ) {
-          newWidth.push(<td id={y + ":" + x}>&#9632;</td>);
+          newWidth.push("&#9632;");
         } else {
-          newWidth.push(<td id={y + ":" + x}>&#183;</td>);
+          newWidth.push("&#183;");
         }
       }
       console.log(`new width ${newWidth}`);
-      newLength.push(<tr>); newLength.push({newWidth}); newLength.push(</tr>);
+      newLength.push({ newWidth });
       newWidth = [];
     }
     console.log(`new room: ${newLength}`);
     newLength[0][1] = "+";
-    return newLength;
-  };
-  return (
-    <div>
-      <h3>Picture of Room</h3>
-      <p>
-        {width}:{length}
-      </p>
-      <table style={tableStyle}>{renderRoom()}</table>
-    </div>
-  );
-};
-
-          y === parseInt(length) + 1 ||
-          x === 0 ||
-          x === parseInt(width) + 1
-        ) {
-          newWidth.push(<td id={y + ":" + x}>&#9632;</td>);
-        } else {
-          newWidth.push(<td id={y + ":" + x}>&#183;</td>);
-        }
-      }
-      console.log(`new width ${newWidth}`);
-      newLength.push(<tr>); newLength.push({newWidth}); newLength.push(</tr>);
-      newWidth = [];
-    }
-    console.log(`new room: ${newLength}`);
-    newLength[0][1] = "+";
-    return newLength;
+    return (
+      <>
+        {newLength.map((row) => (
+          <tr>
+            row.map((item) => (<td>{item}</td>))
+          </tr>
+        ))}
+      </>
+    );
   };
   return (
     <div>
