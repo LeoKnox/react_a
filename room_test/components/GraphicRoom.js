56267da3dc51dbb63f88da5981ecd329e062fc96
LeoKnox <LeoKnox@users.noreply.github.com>
2024-02-08 12:04:39 -0800

    Update GraphicRoom.js

--- room_test/components/GraphicRoom.js
+++ room_test/components/GraphicRoom.js
@@ -1,3 +1,36 @@
-export default GraphicRoom = () => {
-  return <td>Graphic Room</td>;
+export default GraphicRoom = ({ length, width }) => {
+  console.log(`length: ${length} width: ${width}`);
+  let newWidth = [];
+  let newLength = [];
+  console.log(`new width ${newWidth.length} new length ${newLength.length}`);
+  for (let y = 0; y < parseInt(length) + 2; y++) {
+    for (let x = 0; x < parseInt(width) + 2; x++) {
+      if (
+        y === 0 ||
+        y === parseInt(length) + 1 ||
+        x === 0 ||
+        x === parseInt(width) + 1
+      ) {
+        newWidth.push("■");
+      } else {
+        newWidth.push("·");
+      }
+    }
+    console.log(`new width ${JSON.stringify(newWidth)}`);
+    newLength.push(newWidth);
+    newWidth = [];
+  }
+  console.log(`new room: ${JSON.stringify(newLength)}`);
+  //newLength[0][1] = "+";
+  return (
+    <>
+      {newLength.map((row) => (
+        <tr>
+          {row.map((tile) => (
+            <td style={tileStyle}>{tile}</td>
+          ))}
+        </tr>
+      ))}
+    </>
+  );
 };
