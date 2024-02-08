89a917bbdcd69a4beed43fddcd2d9168e606a284
LeoKnox <LeoKnox@users.noreply.github.com>
2024-02-08 12:08:02 -0800

    Update DrawRoom.js

--- room_test/components/DrawRoom.js
+++ room_test/components/DrawRoom.js
@@ -9,47 +9,6 @@ export default DrawRoom = ({ width, length }) => {
     marginLeft: "auto",
     marginRight: "auto",
   };
-  const tileStyle = {
-    backgroundColor: "lightgray",
-    width: "20px",
-    height: "20px",
-  };
-  const renderRoom = () => {
-    console.log(`render room ${length} width ${width}`);
-    let newWidth = [];
-    let newLength = [];
-    console.log(`new width ${newWidth.length} new length ${newLength.length}`);
-    for (let y = 0; y < parseInt(length) + 2; y++) {
-      for (let x = 0; x < parseInt(width) + 2; x++) {
-        if (
-          y === 0 ||
-          y === parseInt(length) + 1 ||
-          x === 0 ||
-          x === parseInt(width) + 1
-        ) {
-          newWidth.push("■");
-        } else {
-          newWidth.push("·");
-        }
-      }
-      console.log(`new width ${JSON.stringify(newWidth)}`);
-      newLength.push(newWidth);
-      newWidth = [];
-    }
-    console.log(`new room: ${JSON.stringify(newLength)}`);
-    newLength[0][1] = "+";
-    return (
-      <>
-        {newLength.map((row) => (
-          <tr>
-            {row.map((tile) => (
-              <td style={tileStyle}>{tile}</td>
-            ))}
-          </tr>
-        ))}
-      </>
-    );
-  };
   return (
     <div>
       <h3>Picture of Room</h3>
@@ -57,7 +16,7 @@ export default DrawRoom = ({ width, length }) => {
         {width}:{length}
       </p>
       <table style={tableStyle}>
-        <GraphicRoom />>
+        <GraphicRoom length={length} width={width} />
       </table>
     </div>
   );
