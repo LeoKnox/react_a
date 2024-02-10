5263a4aa8e65d5242d82fa24894cd39bd7939434
LeoKnox <LeoKnox@users.noreply.github.com>
2024-02-10 11:43:48 -0800

    Update GraphicRoom.js

--- room_test/components/GraphicRoom.js
+++ room_test/components/GraphicRoom.js
@@ -2,14 +2,12 @@ export default GraphicRoom = ({ length, width }) => {
   const tileStyle = {
     backgroundColor: "lightgray",
     fontSize: "1.3em",
-    width: "1.4em",
-    height: "1.4em",
+    fontWeight: "bold",
+    width: "1.1em",
+    height: "1.1em",
   };
-
-  console.log(`length: ${length} width: ${width}`);
   let newWidth = [];
   let newLength = [];
-  console.log(`new width ${newWidth.length} new length ${newLength.length}`);
   for (let y = 0; y < parseInt(length) + 2; y++) {
     for (let x = 0; x < parseInt(width) + 2; x++) {
       if (
@@ -23,18 +21,18 @@ export default GraphicRoom = ({ length, width }) => {
         newWidth.push("·");
       }
     }
-    console.log(`new width ${JSON.stringify(newWidth)}`);
     newLength.push(newWidth);
     newWidth = [];
   }
-  console.log(`new room: ${JSON.stringify(newLength)}`);
   newLength[0][1] = "+";
   return (
     <>
-      {newLength.map((row) => (
+      {newLength.map((row, y) => (
         <tr>
-          {row.map((tile) => (
-            <td style={tileStyle}>{tile}</td>
+          {row.map((tile, x) => (
+            <td style={tileStyle} id={y + "," + x}>
+              {tile}
+            </td>
           ))}
         </tr>
       ))}
