d8865c33eb2960a6935ec8ec26288aa2446a171c
LeoKnox <LeoKnox@users.noreply.github.com>
2024-02-10 11:45:22 -0800

    Update Single.js

--- room_test/components/Single.js
+++ room_test/components/Single.js
@@ -1,44 +1,128 @@
-export default GraphicRoom = ({ length, width }) => {
-  const tileStyle = {
-    backgroundColor: "lightgray",
-    fontSize: "1.3em",
-    fontWeight: "bold",
-    width: "1.1em",
-    height: "1.1em",
+import { useState } from "react";
+import "./DrawRoom.js";
+
+export default Single = ({ roomId, room, setRoomId, updateRoom }) => {
+  const dataStyle = {
+    backgroundColor: "lightblue",
+    height: "8em",
+    padding: "1em",
+    width: "50%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "1em",
   };
-  let newWidth = [];
-  let newLength = [];
-  for (let y = 0; y < parseInt(length) + 2; y++) {
-    for (let x = 0; x < parseInt(width) + 2; x++) {
-      if (
-        y === 0 ||
-        y === parseInt(length) + 1 ||
-        x === 0 ||
-        x === parseInt(width) + 1
-      ) {
-        newWidth.push("■");
-      } else {
-        newWidth.push("·");
-      }
+  const inputStyle = {
+    width: "3em",
+  };
+  const [newRoom, setNewRoom] = useState(room);
+  const [isEdit, setIsEdit] = useState(false);
+  const [sizeError, setSizeError] = useState(false);
+  const changeValue = (e) => {
+    const { name, value } = e.target;
+    setNewRoom((item) => ({
+      ...item,
+      ...item.newRoom,
+      [name]: value,
+    }));
+  };
+  const changeRoom = () => {
+    if (newRoom["width"] > 1 && newRoom["length"] > 1) {
+      updateRoom(newRoom);
+      setIsEdit(false);
+      setSizeError(false);
+    } else {
+      setSizeError(true);
     }
-    newLength.push(newWidth);
-    newWidth = [];
-  }
-  newLength[0][1] = "+";
+  };
+  const resetRoom = () => {
+    console.log(`reset room ${room.name}`);
+    setNewRoom(room);
+  };
   return (
     <>
-      {newLength.map((row) => (
-        <tr>
-          {row.map((tile) => (
-            <td
-              style={tileStyle}
-              onClick={() => (addMonster ? placeMonster() : null)}
-            >
-              {tile}
-            </td>
-          ))}
-        </tr>
-      ))}
+      <h3>Single Room {roomId} </h3>
+      <button onClick={() => setRoomId(-1)}>Home</button>
+      <div style={dataStyle}>
+        {isEdit ? (
+          <p>
+            Name:&nbsp;
+            <input
+              type="text"
+              name="name"
+              autoFocus
+              value={newRoom["name"]}
+              onChange={changeValue}
+            />
+          </p>
+        ) : (
+          <p>{room["name"]}</p>
+        )}
+        <p>
+          {isEdit ? (
+            <>
+              Description:{" "}
+              <input
+                type="text"
+                name="description"
+                value={newRoom["description"]}
+                onChange={changeValue}
+              />
+            </>
+          ) : (
+            <>Description: {room["description"]}</>
+          )}
+        </p>
+        <p>
+          {isEdit ? (
+            <>
+              Width:{" "}
+              <input
+                style={inputStyle}
+                type="number"
+                name="width"
+                value={newRoom["width"]}
+                onChange={changeValue}
+              />
+            </>
+          ) : (
+            <>Width: {room["width"]}</>
+          )}{" "}
+          {isEdit ? (
+            <>
+              Length:{" "}
+              <input
+                style={inputStyle}
+                type="number"
+                name="length"
+                value={newRoom["length"]}
+                onChange={changeValue}
+              />
+            </>
+          ) : (
+            <>Length: {room["length"]}</>
+          )}
+        </p>
+      </div>
+      <p>
+        {isEdit ? (
+          <button onClick={() => setIsEdit(false)}>Cancel</button>
+        ) : (
+          <button onClick={() => setIsEdit(true)}>Edit</button>
+        )}
+        <button>Add Monster</button>
+        {isEdit ? (
+          <p>
+            <button onClick={resetRoom}>Reset</button>
+            <button onClick={changeRoom}>Update</button>
+          </p>
+        ) : null}
+      </p>
+      {sizeError ? (
+        <p className="error">
+          Rooms must be length and width must be greater then 1
+        </p>
+      ) : null}
+      <DrawRoom width={room["width"]} length={room["length"]} />
     </>
   );
 };
