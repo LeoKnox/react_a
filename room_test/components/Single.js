9f149d3e24e5abbe1a92edcf61267f55953ebf6e
LeoKnox <LeoKnox@users.noreply.github.com>
2024-02-27 10:57:42 -0800

    Update Single.js

--- room_test/components/Single.js
+++ room_test/components/Single.js
@@ -1,101 +1,181 @@
-import { memo, useState } from "react";
+import { useState, useEffect } from "react";
+import "./DrawRoom.js";
 
-export default Home = ({ myRooms, addRoom, deleteRoom, setRoomId }) => {
-  const [roomObj, setRoomObj] = useState({});
-  const [homeErrorList, setHomeErrorList] = useState({});
-  const updateRoom = (e) => {
+export default Single = ({
+  roomId,
+  room,
+  setRoomId,
+  updateRoom,
+  pushMonster,
+  newMonster,
+  setNewMonster,
+  errorList,
+  setErrorList,
+}) => {
+  const dataStyle = {
+    backgroundColor: "lightblue",
+    height: "8em",
+    padding: "1em",
+    width: "50%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "1em",
+  };
+  const inputStyle = {
+    width: "3em",
+  };
+  const [newRoom, setNewRoom] = useState(room);
+  const [isEdit, setIsEdit] = useState(false);
+  const [sizeError, setSizeError] = useState(false);
+  const [nameError, setNameError] = useState(false);
+  const [addMonster, setAddMonster] = useState(false);
+  const [minRoomX, setMinRoomX] = useState();
+  const [minRoomY, setMinRoomY] = useState();
+  useEffect(() => {
+    if (newRoom["monsters"].length === 0) {
+      setMinRoomX(0);
+      setMinRoomY(0);
+    } else {
+      setMinRoomX(
+        newRoom["monsters"].reduce((prev, current) =>
+          prev && prev["x"] > current["x"] ? prev : current
+        )["x"]
+      );
+      setMinRoomY(
+        newRoom["monsters"].reduce((prev, current) =>
+          prev && prev["y"] > current["y"] ? prev : current
+        )["y"]
+      );
+    }
+  }, [pushMonster]);
+  const changeValue = (e) => {
     const { name, value } = e.target;
-    setRoomObj((item) => ({
+    setNewRoom((item) => ({
       ...item,
-      ...item.roomObj,
+      ...item.newRoom,
       [name]: value,
     }));
   };
-  const setRoom = () => {
-    let tempError = {};
-    if (!roomObj["name"] || roomObj["name"].length < 3) {
-      tempError["shortName"] = true;
-    }
-    if (roomObj["width"] < 1) {
-      tempError["lowWidth"] = true;
-    }
-    if (roomObj["length"] < 1) {
-      tempError["lowLength"] = true;
-    }
-    if (Object.keys(tempError).length < 1) {
-      roomObj["length"] = +roomObj["length"];
-      roomObj["width"] = +roomObj["width"];
-      addRoom(roomObj);
-      setRoomObj({ name: "", description: "", length: 0, width: 0 });
-      for (key in tempError) {
-        delete tempError[key];
-      }
-      setHomeErrorList(tempError);
+  const changeRoom = () => {
+    if (
+      newRoom["width"] > 0 &&
+      newRoom["length"] > 0 &&
+      newRoom["name"].length > 2
+    ) {
+      updateRoom(newRoom);
+      setIsEdit(false);
+      setSizeError(false);
+      setNameError(false);
     } else {
-      setHomeErrorList(tempError);
+      if (newRoom["name"].length < 3) {
+        setNameError(true);
+      } else {
+        setNameError(false);
+      }
+      setSizeError(true);
     }
+  };
+  const resetRoom = () => {
+    setNewRoom(room);
   };
   return (
     <>
-      <h1>Rooms</h1>
-      {myRooms.map((room) => {
-        return (
-          <>
-            <p>
-              <button onClick={() => setRoomId(room["id"])}>
-                {room["name"]}
-              </button>
-              <button onClick={() => deleteRoom(room["id"])}>delete</button>
-            </p>
-            <p>{room["description"]}</p>
-          </>
-        );
-      })}
+      <h3>Single Room</h3>
+      <button onClick={() => setRoomId(-1)}>Home</button>
+      <div style={dataStyle}>
+        {isEdit ? (
+          <p>
+            Name:&nbsp;
+            <input
+              type="text"
+              name="name"
+              autoFocus
+              value={newRoom["name"]}
+              onChange={changeValue}
+            />
+          </p>
+        ) : (
+          <p>{room["name"]}</p>
+        )}
+        <p>
+          {isEdit ? (
+            <>
+              Description:{" "}
+              <input
+                type="text"
+                name="description"
+                value={newRoom["description"]}
+                onChange={changeValue}
+              />
+            </>
+          ) : (
+            <>Description: {room["description"]}</>
+          )}
+        </p>
+        <p>
+          {isEdit ? (
+            <>
+              Width:{" "}
+              <input
+                style={inputStyle}
+                type="number"
+                name="width"
+                value={newRoom["width"]}
+                min={newMonster["x"] < minRoomX ? minRoomX : newMonster["x"]}
+                onChange={changeValue}
+              />
+            </>
+          ) : (
+            <>Width: {room["width"]}</>
+          )}{" "}
+          {isEdit ? (
+            <>
+              Length:{" "}
+              <input
+                style={inputStyle}
+                type="number"
+                name="length"
+                value={newRoom["length"]}
+                min={newMonster["y"] < minRoomY ? minRoomY : newMonster["y"]}
+                onChange={changeValue}
+              />
+            </>
+          ) : (
+            <>Length: {room["length"]}</>
+          )}
+        </p>
+      </div>
       <p>
-        Name:
-        <input
-          type="text"
-          name="name"
-          autoFocus
-          value={roomObj["name"]}
-          onChange={updateRoom}
-        />
+        {isEdit ? (
+          <button onClick={() => setIsEdit(false)}>Cancel</button>
+        ) : (
+          <button onClick={() => setIsEdit(true)}>Edit</button>
+        )}
+        <button onClick={() => setAddMonster(!addMonster)}>Add Monster</button>
+        <button onClick={() => pushMonster(newMonster, roomId)}>Push</button>
+        {isEdit ? (
+          <p>
+            <button onClick={resetRoom}>Reset</button>
+            <button onClick={changeRoom}>Update</button>
+          </p>
+        ) : null}
       </p>
-      {homeErrorList["shortName"] ? (
-        <p>name must be 3 characters or longer</p>
+      {sizeError ? (
+        <p className="error">
+          Rooms must be length and width must be at least 1
+        </p>
       ) : null}
-      <p>
-        description:
-        <input
-          type="text"
-          name="description"
-          value={roomObj["description"]}
-          onChange={updateRoom}
-        />
-      </p>
-      <p>
-        Length:
-        <input
-          type="number"
-          name="length"
-          value={roomObj["length"] ? roomObj["length"] : 1}
-          onChange={updateRoom}
-        />
-      </p>
-      {homeErrorList["lowLength"] ? <p>Length must be higher then 1</p> : null}
-      <p>
-        Width:
-        <input
-          type="number"
-          name="width"
-          value={roomObj["width"] ? roomObj["width"] : 1}
-          onChange={updateRoom}
-        />
-      </p>
-      {homeErrorList["lowWidth"] ? <p>Width must be higher then 1</p> : null}
-      <p>
-        <button onClick={setRoom}>set room</button>
-      </p>
+      <DrawRoom
+        width={newRoom["width"]}
+        length={newRoom["length"]}
+        addMonster={addMonster}
+        setAddMonster={setAddMonster}
+        newMonster={newMonster}
+        setNewMonster={setNewMonster}
+        monsters={room["monsters"]}
+        errorList={errorList}
+        setErrorList={setErrorList}
+      />
     </>
   );
 };
