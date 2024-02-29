23a59c97463f855b1c5e190031ebe1938fb8ca7e
LeoKnox <LeoKnox@users.noreply.github.com>
2024-02-29 11:12:51 -0800

    Update DrawRoom.js

--- room_test/components/DrawRoom.js
+++ room_test/components/DrawRoom.js
@@ -24,7 +24,7 @@ export default function App() {
   });
   const slayMonster = (id) => {
     alert("slay!" + JSON.stringify(myRooms[id]["monsters"]));
-    myRooms[id]["monsters"].splice(id, 1);
+    return myRooms[id]["monsters"].splice(id, 1);
     //console.log(JSON.stringify(room["monsters"]));
     //setNewMonster(monsters.filter((monster, index) => index !== id));
   };
